5d3138a152ad32ea3082c33aecb94cfe8c172648
emtkmkk <emtk@mkkey.net>
2023-04-18 06:09:50 +0000

    ローカルにフォロワーがいない場合のみプロキシアカウントを動作させる

--- packages/backend/src/services/user-list/push.ts
+++ packages/backend/src/services/user-list/push.ts
@@ -1,7 +1,7 @@
 import { publishUserListStream } from "@/services/stream.js";
 import type { User } from "@/models/entities/user.js";
 import type { UserList } from "@/models/entities/user-list.js";
-import { UserListJoinings, Users } from "@/models/index.js";
+import { UserListJoinings, Users, Followings } from "@/models/index.js";
 import type { UserListJoining } from "@/models/entities/user-list-joining.js";
 import { genId } from "@/misc/gen-id.js";
 import { fetchProxyAccount } from "@/misc/fetch-proxy-account.js";
@@ -16,9 +16,15 @@ export async function pushUserToUserList(target: User, list: UserList) {
 	} as UserListJoining);
 
 	publishUserListStream(list.id, "userAdded", await Users.pack(target));
+	
+	const localFollowersCount = Followings.createQueryBuilder("following")
+			.where("following.followeeId = :userId", { userId: target.id })
+			.andWhere("following.followerHost IS NULL")
+			.getCount()
 
 	// このインスタンス内にこのリモートユーザーをフォローしているユーザーがいなくても投稿を受け取るためにダミーのユーザーがフォローしたということにする
-	if (Users.isRemoteUser(target)) {
+	// 条件 : 対象のローカル内フォロワーが0人以下（ローカルからフォローされていない）
+	if (Users.isRemoteUser(target) && localFollowersCount <= 0) {
 		const proxy = await fetchProxyAccount();
 		if (proxy) {
 			createFollowing(proxy, target);

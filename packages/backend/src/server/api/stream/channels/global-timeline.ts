1c59404dab5b35744f6bd813e974d614656ba17f
emtkmkk <emtk@mkkey.net>
2023-04-19 05:33:21 +0000

    'packages/backend/src/server/api/stream/channels/global-timeline.ts' を更新

--- packages/backend/src/server/api/stream/channels/global-timeline.ts
+++ packages/backend/src/server/api/stream/channels/global-timeline.ts
@@ -33,8 +33,9 @@ export default class extends Channel {
 		// 関係ない返信は除外
 		if (note.reply && !this.user!.showTimelineReplies) {
 			const reply = note.reply;
-			// 「チャンネル接続主への返信」でもなければ、「チャンネル接続主が行った返信」でもなければ、「投稿者の投稿者自身への返信」でもない場合
+			// 「フォロー中同士の会話」でもなければ、「チャンネル接続主への返信」でもなければ、「チャンネル接続主が行った返信」でもなければ、「投稿者の投稿者自身への返信」でもない場合
 			if (
+				!(this.following.has(reply.userId) && this.following.has(note.userId)) &&
 				reply.userId !== this.user!.id &&
 				note.userId !== this.user!.id &&
 				reply.userId !== note.userId

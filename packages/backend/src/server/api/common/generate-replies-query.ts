494f0e2398c2fd0bd0cdbcbc46584e2b05f2b9e0
emtkmkk <emtk@mkkey.net>
2023-04-19 06:23:08 +0000

    クエリを追加

--- packages/backend/src/server/api/common/generate-replies-query.ts
+++ packages/backend/src/server/api/common/generate-replies-query.ts
@@ -5,6 +5,7 @@ import { Brackets } from "typeorm";
 export function generateRepliesQuery(
 	q: SelectQueryBuilder<any>,
 	me?: Pick<User, "id" | "showTimelineReplies"> | null,
+	following?: String | null,
 ) {
 	if (me == null) {
 		q.andWhere(
@@ -21,27 +22,61 @@ export function generateRepliesQuery(
 			}),
 		);
 	} else if (!me.showTimelineReplies) {
-		q.andWhere(
-			new Brackets((qb) => {
-				qb.where("note.replyId IS NULL") // 返信ではない
-					.orWhere("note.replyUserId = :meId", { meId: me.id }) // 返信だけど自分のノートへの返信
-					.orWhere(
-						new Brackets((qb) => {
-							qb.where(
-								// 返信だけど自分の行った返信
-								"note.replyId IS NOT NULL",
-							).andWhere("note.userId = :meId", { meId: me.id });
-						}),
-					)
-					.orWhere(
-						new Brackets((qb) => {
-							qb.where(
-								// 返信だけど投稿者自身への返信
-								"note.replyId IS NOT NULL",
-							).andWhere("note.replyUserId = note.userId");
-						}),
-					);
-			}),
-		);
+		if (following != null) {
+			q.andWhere(
+				new Brackets((qb) => {
+					qb.where("note.replyId IS NULL") // 返信ではない
+						.orWhere("note.replyUserId = :meId", { meId: me.id }) // 返信だけど自分のノートへの返信
+						.orWhere(
+							new Brackets((qb) => {
+								qb.where(
+									// 返信だけど自分の行った返信
+									"note.replyId IS NOT NULL",
+								).andWhere("note.userId = :meId", { meId: me.id });
+							}),
+						)
+						.orWhere(
+							new Brackets((qb) => {
+								qb.where(
+									// 返信だけど投稿者自身への返信
+									"note.replyId IS NOT NULL",
+								).andWhere("note.replyUserId = note.userId");
+							}),
+						)
+						.orWhere(
+							new Brackets((qb) => {
+								qb.where(
+									// 返信だけどノート主、返信先をフォローしている
+									"note.replyId IS NOT NULL",
+								).andWhere("note.replyUserId IN (:followId)", { followId: following }
+								).andWhere("note.userId IN (:followId)", { followId: following });
+							}),
+						)
+				}),
+			);
+		} else {
+			q.andWhere(
+				new Brackets((qb) => {
+					qb.where("note.replyId IS NULL") // 返信ではない
+						.orWhere("note.replyUserId = :meId", { meId: me.id }) // 返信だけど自分のノートへの返信
+						.orWhere(
+							new Brackets((qb) => {
+								qb.where(
+									// 返信だけど自分の行った返信
+									"note.replyId IS NOT NULL",
+								).andWhere("note.userId = :meId", { meId: me.id });
+							}),
+						)
+						.orWhere(
+							new Brackets((qb) => {
+								qb.where(
+									// 返信だけど投稿者自身への返信
+									"note.replyId IS NOT NULL",
+								).andWhere("note.replyUserId = note.userId");
+							}),
+						);
+				}),
+			);
+		}
 	}
 }

f626467c1d04660ee8270e55bb5c4c4b2b42bbbc
emtkmkk <emtk@mkkey.net>
2023-04-20 01:53:59 +0000

    言語取得失敗時に日本語を返す

--- packages/client/src/config.ts
+++ packages/client/src/config.ts
@@ -10,7 +10,7 @@ export const apiUrl = `${url}/api`;
 export const wsUrl = `${url
 	.replace("http://", "ws://")
 	.replace("https://", "wss://")}/streaming`;
-export const lang = localStorage.getItem("lang");
+export const lang = localStorage.getItem("lang") ?? "ja-JP";
 export const langs = _LANGS_;
 export const locale = JSON.parse(localStorage.getItem("locale"));
 export const version = _VERSION_;

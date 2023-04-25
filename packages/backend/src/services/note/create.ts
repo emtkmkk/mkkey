2a5db85cde6fabc255c08a04d0c90ac04b8a8670
emtkmkk <emtk@mkkey.net>
2023-04-25 09:32:01 +0000

    'packages/backend/src/services/note/create.ts' を更新

--- packages/backend/src/services/note/create.ts
+++ packages/backend/src/services/note/create.ts
@@ -198,6 +198,9 @@ export default async (
 		if (data.channel != null && data.localOnly === false) {
 			//チャンネルで連合有りの場合、ハッシュタグを自動で付ける
 			data.text += " #" + data.channel!.name;
+			if (data.apHashtags == null){
+				data.apHashtags = [];
+			}
 			data.apHashtags.push(data.channel!.name);
 		}
 

f3df9186dafa0bc6bfca949ab6272ccd0be5a032
emtkmkk <emtk@mkkey.net>
2023-04-26 23:40:11 +0000

    ソフトウェア名と一緒にバージョンも表示する

--- packages/client/src/components/MkInstanceTicker.vue
+++ packages/client/src/components/MkInstanceTicker.vue
@@ -35,7 +35,7 @@ const instance = props.instance ?? {
 			'meta[name="theme-color-orig"]'
 		) as HTMLMetaElement
 	)?.content,
-	softwareName: Instance.softwareName || "???",
+	softwareName: (Instance.softwareName || "???") + "/" + (instance.softwareVersion || "???"),
 };
 
 const capitalize = (s: string) => s && s[0].toUpperCase() + s.slice(1);

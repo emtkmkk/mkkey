d6c114a602318bc720884d7dbea71ab677e28786
emtkmkk <emtk@mkkey.net>
2023-05-08 05:57:40 +0000

    よく考えたら無理だった

--- packages/backend/src/server/api/stream/channels/channel.ts
+++ packages/backend/src/server/api/stream/channels/channel.ts
@@ -11,6 +11,7 @@ export default class extends Channel {
 	public static shouldShare = false;
 	public static requireCredential = false;
 	private channelId: string;
+	private channelName: string;
 	private typers: Map<User["id"], Date> = new Map();
 	private emitTypersIntervalId: ReturnType<typeof setInterval>;
 
@@ -33,7 +34,7 @@ export default class extends Channel {
 
 	private async onNote(note: Packed<"Note">) {
 		if (note.visibility !== "public") return;
-		if (note.channelId !== this.channelId && (!note.channel || !note.tag.includes(normalizeForSearch(note.channel.name)))) return;
+		if (note.channelId !== this.channelId && (!this.channelName || !note.tag.includes(normalizeForSearch(this.channelName)))) return;
 
 		// 流れてきたNoteがミュートしているユーザーが関わるものだったら無視する
 		if (isUserRelated(note, this.muting)) return;

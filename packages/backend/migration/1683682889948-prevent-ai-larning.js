tolehata/hataskey

feat: 投稿したコンテンツのAIによる学習を軽減するオプションを追加
Resolve #10819

packages/backend/migration/1683682889948-prevent-ai-larning.js
@@ -1,0 +1,11 @@
+export class PreventAiLarning1683682889948 {
+    name = 'PreventAiLarning1683682889948'
+
+    async up(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "user_profile" ADD "preventAiLarning" boolean NOT NULL DEFAULT true`);
+    }
+
+    async down(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "user_profile" DROP COLUMN "preventAiLarning"`);
+    }
+}

packages/frontend/src/pages/settings/privacy.vue
@@ -24,6 +24,10 @@
 		{{ i18n.ts.noCrawle }}
 		<template #caption>{{ i18n.ts.noCrawleDescription }}</template>
 	</MkSwitch>
+	<MkSwitch v-model="preventAiLarning" @update:model-value="save()">
+		{{ i18n.ts.preventAiLarning }}<span class="_beta">{{ i18n.ts.beta }}</span>
+		<template #caption>{{ i18n.ts.preventAiLarningDescription }}</template>
+	</MkSwitch>
 	<MkSwitch v-model="isExplorable" @update:model-value="save()">
 		{{ i18n.ts.makeExplorable }}
 		<template #caption>{{ i18n.ts.makeExplorableDescription }}</template>
@@ -71,6 +75,7 @@ import { definePageMetadata } from '@/scripts/page-metadata';
 let isLocked = $ref($i.isLocked);
 let autoAcceptFollowed = $ref($i.autoAcceptFollowed);
 let noCrawle = $ref($i.noCrawle);
+let preventAiLarning = $ref($i.preventAiLarning);
 let isExplorable = $ref($i.isExplorable);
 let hideOnlineStatus = $ref($i.hideOnlineStatus);
 let publicReactions = $ref($i.publicReactions);
@@ -86,6 +91,7 @@ function save() {
 		isLocked: !!isLocked,
 		autoAcceptFollowed: !!autoAcceptFollowed,
 		noCrawle: !!noCrawle,
+		preventAiLarning: !!preventAiLarning,
 		isExplorable: !!isExplorable,
 		hideOnlineStatus: !!hideOnlineStatus,
 		publicReactions: !!publicReactions,

emtkmkk/mkkey

Co-authored-by: GitHub <syuilo>

packages/backend/migration/1683682889948-prevent-ai-larning.js
@@ -1,0 +1,11 @@
+export class PreventAiLarning1683682889948 {
+    name = 'PreventAiLarning1683682889948'
+
+    async up(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "user_profile" ADD "preventAiLearning" boolean NOT NULL DEFAULT true`);
+    }
+
+    async down(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "user_profile" DROP COLUMN "preventAiLearning"`);
+    }
+}

packages/client/src/pages/settings/privacy.vue
@@ -60,6 +60,10 @@
 			{{ i18n.ts.noCrawle }}
 			<template #caption>{{ i18n.ts.noCrawleDescription }}</template>
 		</FormSwitch>
+		<FormSwitch v-model="preventAiLearning" @update:model-value="save()">
+			{{ i18n.ts.preventAiLearning }}<span class="_beta">{{ i18n.ts.beta }}</span>
+			<template #caption>{{ i18n.ts.preventAiLearningDescription }}</template>
+		</FormSwitch>
 		<FormSwitch
 			v-model="isExplorable"
 			class="_formBlock"
@@ -145,6 +149,7 @@ let isExplorable = $ref($i.isExplorable);
 let hideOnlineStatus = $ref($i.hideOnlineStatus);
 let publicReactions = $ref($i.publicReactions);
 let ffVisibility = $ref($i.ffVisibility);
+let preventAiLearning = $ref($i.preventAiLearning);
 
 let defaultNoteVisibility = $computed(
 	defaultStore.makeGetterSetter("defaultNoteVisibility")
@@ -165,6 +170,7 @@ function save() {
 		isExplorable: !!isExplorable,
 		hideOnlineStatus: !!hideOnlineStatus,
 		publicReactions: !!publicReactions,
+		preventAiLearning: !!preventAiLearning,
 		ffVisibility: ffVisibility,
 	});
 }
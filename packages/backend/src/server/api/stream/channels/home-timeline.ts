fe53462a116415dbca4b65ba5bec1ef90e04b897
emtkmkk <emtk@mkkey.net>
2023-05-23 01:46:52 +0000

    streamにも遡りを追加

--- packages/backend/src/server/api/stream/channels/home-timeline.ts
+++ packages/backend/src/server/api/stream/channels/home-timeline.ts
@@ -42,12 +42,13 @@ export default class extends Channel {
 		// 関係ない返信は除外
 		if (note.reply && !this.user!.showTimelineReplies) {
 			const reply = note.reply;
-			// 「フォロー中同士の会話」でもなければ、「チャンネル接続主への返信」でもなければ、「チャンネル接続主が行った返信」でもなければ、「投稿者の投稿者自身への返信」でもない場合
+			// 「フォロー中同士の会話」でもなければ、「チャンネル接続主への返信」でもなければ、「チャンネル接続主が行った返信」でもなければ、「投稿者の投稿者自身への返信（ただし一つ上の投稿へ遡る）」でもない場合
 			if (
 				!(this.following.has(reply.userId) && this.following.has(note.userId)) &&
 				reply.userId !== this.user!.id &&
 				note.userId !== this.user!.id &&
-				reply.userId !== note.userId
+				reply.userId !== note.userId &&
+				(reply.replyUserId !== note.userId && !(this.following.has(reply.replyUserId) && this.following.has(note.userId)))
 			)
 				return;
 		}

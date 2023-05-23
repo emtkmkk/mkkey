c6cf131969f0721ec7a713242fc66aecaa7338ac
emtkmkk <emtk@mkkey.net>
2023-05-23 01:37:24 +0000

    自己返信の場合、返信先の返信ユーザーをフォローしているか確認する
    A - B - B の形のリプライツリーの時にB - Bの所で自己返信となり、Bをフォローしているだけで見えてしまうことの対策
    B - B の場合に x - B - B の x を確認しに行くようにした
    
    A - B - B - B の場合はすり抜けるがまあもう良いでしょう

--- packages/backend/src/server/api/common/generate-replies-query.ts
+++ packages/backend/src/server/api/common/generate-replies-query.ts
@@ -16,7 +16,8 @@ export function generateRepliesQuery(
 							qb.where(
 								// 返信だけど投稿者自身への返信
 								"note.replyId IS NOT NULL",
-							).andWhere("note.replyUserId = note.userId");
+							).andWhere("(note.replyUserId = note.userId)"
+							).andWhere("((reply.replyUserId IS NULL) OR (reply.replyUserId = note.userId))");
 						}),
 					);
 			}),
@@ -39,8 +40,10 @@ export function generateRepliesQuery(
 							new Brackets((qb) => {
 								qb.where(
 									// 返信だけど投稿者自身への返信
+									// ただし一つ前の投稿の返信へ遡ってチェックを行う
 									"note.replyId IS NOT NULL",
-								).andWhere("note.replyUserId = note.userId");
+								).andWhere("note.replyUserId = note.userId"
+								).andWhere(`((reply.replyUserId IS NULL) OR (reply.replyUserId = note.userId) OR (reply.replyUserId IN (${following}) AND note.userId IN (${following})))`);
 							}),
 						)
 						.orWhere(

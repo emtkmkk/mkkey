47222639a11569a2d444db011131950667335286
emtkmkk <emtk@mkkey.net>
2023-05-27 01:25:50 +0000

    'packages/client/src/components/MkNote.vue' を更新

--- packages/client/src/components/MkNote.vue
+++ packages/client/src/components/MkNote.vue
@@ -7,7 +7,7 @@
 		v-size="{ max: [500, 450, 350, 300] }"
 		class="tkcbzcuz"
 		:tabindex="!isDeleted ? '-1' : null"
-		:class="[{ renote: isRenote } , v-${appearNote.visibility} , l-${appearNote.localOnly} ]"
+		:class="[{ renote: isRenote } , `v-${appearNote.visibility}` , `l-${appearNote.localOnly}` ]"
 	>
 		<MkNoteSub
 			v-if="appearNote.reply && !detailedView"

9ad1b09fb1ba435b4c33faf26d616c2baf379889
emtkmkk <emtk@mkkey.net>
2023-06-02 03:42:37 +0000

    'packages/client/src/components/MkPostForm.vue' を更新

--- packages/client/src/components/MkPostForm.vue
+++ packages/client/src/components/MkPostForm.vue
@@ -557,7 +557,7 @@ let recentHashtags = $ref(JSON.parse(localStorage.getItem("hashtags") || "[]"));
 let canPublic = $ref((!props.reply || props.reply.visibility === "public") && (!props.renote || props.renote.visibility === "public"));
 let canHome = $ref((!props.reply || (props.reply.visibility === "public" || props.reply.visibility === "home")) && (!props.renote || (props.renote.visibility === "public" || props.renote.visibility === "home")));
 let canFollower = $ref((!props.reply || props.reply.visibility !== "specified") && (!props.renote || props.renote.visibility !== "specified"));
-let canNotLocal = $ref((!props.reply || props.reply.localOnly == false) && (!props.renote || props.renote.localOnly == false));
+let canNotLocal = $ref((!props.reply || !props.reply.localOnly) && (!props.renote || !props.renote.localOnly));
 let imeText = $ref("");
 
 const publicIcon = $computed((): String => {

558f365a72c5caa97ad87743dff7e8b603a6ce51
emtkmkk <emtk@mkkey.net>
2023-06-05 00:25:46 +0000

    'packages/backend/migration/1683682889949-addUserSetting.ts' を更新

--- packages/backend/migration/1683682889949-addUserSetting.ts
+++ packages/backend/migration/1683682889949-addUserSetting.ts
@@ -4,10 +4,10 @@ export class addUserSetting1683682889949 implements MigrationInterface {
     name = 'addUserSetting1683682889949'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" ADD "blockPostPublic" boolean NOT NULL DEFAULT true`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "blockPostHome" boolean NOT NULL DEFAULT true`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "blockPostNotLocal" boolean NOT NULL DEFAULT true`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "isSilentLocked" boolean NOT NULL DEFAULT true`);
+        await queryRunner.query(`ALTER TABLE "user" ADD "blockPostPublic" boolean NOT NULL DEFAULT false`);
+        await queryRunner.query(`ALTER TABLE "user" ADD "blockPostHome" boolean NOT NULL DEFAULT false`);
+        await queryRunner.query(`ALTER TABLE "user" ADD "blockPostNotLocal" boolean NOT NULL DEFAULT false`);
+        await queryRunner.query(`ALTER TABLE "user" ADD "isSilentLocked" boolean NOT NULL DEFAULT false`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {

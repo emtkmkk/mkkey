8e978a092814a841bd8e3005c8821ec0ec846716
emtkmkk <emtk@mkkey.net>
2023-06-06 02:34:05 +0000

    'packages/backend/migration/1683682889949-addUserSetting.ts' を更新

--- packages/backend/migration/1683682889949-addUserSetting.ts
+++ packages/backend/migration/1683682889949-addUserSetting.ts
@@ -7,6 +7,7 @@ export class addUserSetting1683682889949 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "user" ADD "blockPostPublic" boolean NOT NULL DEFAULT false`);
         await queryRunner.query(`ALTER TABLE "user" ADD "blockPostHome" boolean NOT NULL DEFAULT false`);
         await queryRunner.query(`ALTER TABLE "user" ADD "blockPostNotLocal" boolean NOT NULL DEFAULT false`);
+        await queryRunner.query(`ALTER TABLE "user" ADD "blockPostNotLocalPublic" boolean NOT NULL DEFAULT false`);
         await queryRunner.query(`ALTER TABLE "user" ADD "isSilentLocked" boolean NOT NULL DEFAULT false`);
     }
 
@@ -14,6 +15,7 @@ export class addUserSetting1683682889949 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "blockPostPublic"`);
         await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "blockPostHome"`);
         await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "blockPostNotLocal"`);
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "blockPostNotLocalPublic"`);
         await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "isSilentLocked"`);
     }
 

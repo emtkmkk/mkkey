6ec374f6fe71607e703d5286eef0f17758d50001
emtkmkk <emtk@mkkey.net>
2023-06-06 05:31:47 +0000

    pnameが空白ならばソフトミュートを完全に隠す

--- packages/client/src/components/MkNote.vue
+++ packages/client/src/components/MkNote.vue
@@ -198,7 +198,7 @@
 			</div>
 		</article>
 	</div>
-	<button v-else-if="!hiddenSoftMutes" class="muted _button" @click="muted.muted = false">
+	<button v-else-if="!hiddenSoftMutes && muted.matched.join(", ").length != 0" class="muted _button" @click="muted.muted = false">
 		<I18n :src="softMuteReasonI18nSrc(muted.what)" tag="small">
 			<template #name>
 				<MkA

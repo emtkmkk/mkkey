8b15f2dfd4656f21d6a02fbffba49533d154ac87
emtkmkk <emtk@mkkey.net>
2023-06-08 07:35:48 +0000

    'packages/backend/src/models/entities/webhook.ts' を更新

--- packages/backend/src/models/entities/webhook.ts
+++ packages/backend/src/models/entities/webhook.ts
@@ -54,7 +54,7 @@ export class Webhook {
 	@Column('varchar', {
 		length: 128, array: true, default: '{}',
 	})
-	public on: typeof webhookEventTypes[number][];
+	public on: string[];
 
 	@Column('varchar', {
 		length: 1024,

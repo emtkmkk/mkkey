e58e7875ae7dc8058facc31a07d75927539bee9d
emtkmkk <emtk@mkkey.net>
2023-06-08 04:58:45 +0000

    'packages/client/src/pages/settings/webhook.new.vue' を更新

--- packages/client/src/pages/settings/webhook.new.vue
+++ packages/client/src/pages/settings/webhook.new.vue
@@ -89,9 +89,9 @@ let event_mention = $ref(true);
 let event_antenna = $ref(false);
 
 const antennas_all = await os.api("antennas/list");
-const antennas = antennas_all.filter((x) => {x.notity} )
+const antennas = antennas_all.filter((x) => {x.notity} );
 
-let event_excludeAntennas = $ref(Array(antennas.length).fill(true));
+let event_excludeAntennas = $ref(antennas.map((x) => {true}));
 
 async function create(): Promise<void> {
 	const events = [];

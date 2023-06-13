48be1d765f088d9178e46b0678e1b0d0ba7813b9
emtkmkk <emtk@mkkey.net>
2023-06-13 02:40:55 +0000

    'packages/backend/src/services/note/create.ts' を更新

--- packages/backend/src/services/note/create.ts
+++ packages/backend/src/services/note/create.ts
@@ -208,7 +208,7 @@ export default async (
 		if (user.blockPostHome && data.visibility === "home") data.visibility = "followers";
 		if (user.blockPostNotLocal && data.localOnly === false && (!user.blockPostNotLocalPublic || data.visibility === "public")) data.localOnly = true;
 		if (data.visibility === "specified" && data.localOnly === true) data.localOnly = false;
-		if (data.channel != null && data.channel.description?.include("[localOnly]") && data.localOnly === false) data.localOnly = true;
+		if (data.channel != null && data.channel.description?.includes("[localOnly]") && data.localOnly === false) data.localOnly = true;
 		if (data.channel != null && data.localOnly === false && !data.reply && !data.text.endsWith(" #" + data.channel!.name)) {
 			//チャンネルで連合有りで返信でなく、すでに文末にタグが付いていない場合、ハッシュタグを自動で付ける
 			data.text += " #" + data.channel!.name;

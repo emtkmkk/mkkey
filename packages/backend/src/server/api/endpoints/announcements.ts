0d878363ae4b0bcc43474faaaf9ddec882f31502
emtkmkk <emtk@mkkey.net>
2023-06-14 01:21:37 +0000

    未ログイン状態ではお知らせを取得できないように変更

--- packages/backend/src/server/api/endpoints/announcements.ts
+++ packages/backend/src/server/api/endpoints/announcements.ts
@@ -5,7 +5,7 @@ import { makePaginationQuery } from "../common/make-pagination-query.js";
 export const meta = {
 	tags: ["meta"],
 
-	requireCredential: false,
+	requireCredential: true,
 	requireCredentialPrivateMode: true,
 
 	res: {

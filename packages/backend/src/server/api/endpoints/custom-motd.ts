a4f984dd71955c74f63d52a9250b8b6b972e340f
emtkmkk <emtk@mkkey.net>
2023-06-14 02:05:24 +0000

    ログイン済みかどうかや日付・時刻によって、カスタムmotdを追加する

--- packages/backend/src/server/api/endpoints/custom-motd.ts
+++ packages/backend/src/server/api/endpoints/custom-motd.ts
@@ -26,8 +26,46 @@ export const paramDef = {
 	required: [],
 } as const;
 
-export default define(meta, paramDef, async () => {
+export default define(meta, paramDef, async (ps, user) => {
 	const meta = await fetchMeta();
 	const motd = await Promise.all(meta.customMOTD.map((x) => x));
+	
+	const now = new Date();
+	if (now.getMonth() == 10 && now.getDate() == 26) {
+		return ["今日はもこきー " + (now.getFullYear() - 2022) + " 周年の日です！🎉"];
+	}
+	
+	if (user) {
+		const eDay = Math.ceil((now.valueOf() - new Date(user.createdAt).valueOf()) / (1000 * 60 * 60 * 24));
+		const avePost = Math.round((user.notesCount / eDay) * 10) / 10;
+		const uName = user.name || user.username;
+		
+		if (new Date(user.birthday).getMonth() === now.getMonth() && new Date(user.birthday).getDate() === now.getDate()) {
+			return ["誕生日おめでとうございます！🎉"];
+		}
+		
+		if (now.getHours() >= 5 && now.getHours() <= 10) {
+			motd.push("おはようございます、" + uName + "さん")
+		} else if (now.getHours() >= 11 && now.getHours() <= 15) {
+			motd.push("こんにちは、" + uName + "さん")
+		} else if (now.getHours() >= 16 && now.getHours() <= 18) {
+			if (now.getDay() >= 1 && now.getDay() <= 5){
+				motd.push("お疲れ様です、" + uName + "さん")
+			} else {
+				motd.push("こんにちは、" + uName + "さん")
+			}
+		} else {
+			motd.push("こんばんは、" + uName + "さん")
+		}
+		
+		motd.push("アカウントを作成してから " + eDay + " 日目です")
+		motd.push("あなたの現在のノート数は " + user.notesCount + " です")
+		if (eDay >= 7 && avePost >= 2) motd.push("あなたの一日平均ノート数は " + avePost + " です")
+		
+		if (user.isCat && user.speakAsCat) motd.push("にゃー")
+		if (user.isCat && user.speakAsCat) motd.push("にゃー！")
+		if (user.isCat && user.speakAsCat) motd.push("にゃー……")
+	}
+	
 	return motd;
 });

5db02b3185413776cae4db9ebee38f9a030f4afd
emtkmkk <emtk@mkkey.net>
2023-06-15 08:09:40 +0000

    ランクの判定に使用するパワーは61日以内のイベントのみカウント
    これによって取得処理がさらに重くなる

--- packages/backend/src/server/api/endpoints/users/stats.ts
+++ packages/backend/src/server/api/endpoints/users/stats.ts
@@ -178,9 +178,10 @@ export default define(meta, paramDef, async (ps, me) => {
 	}
 
 	let now = new Date();
-	let mkk = new Date(2023,3,5);
+	let borderDate = new Date();
+	borderDate.setDate(now.getDate() - 61);
 	
-	const elapsedDays = Math.max(Math.min(Math.ceil((now.getTime() - Date.parse(user.createdAt)) / (1000 * 60 * 60 * 2.4)) / 10,Math.ceil((now.getTime() - mkk.getTime()) / (1000 * 60 * 60 * 2.4)) / 10),1);
+	const elapsedDays = Math.max(Math.min(Math.ceil((now.getTime() - Date.parse(user.createdAt)) / (1000 * 60 * 60 * 2.4)) / 10,Math.ceil((now.getTime() - borderDate.getTime()) / (1000 * 60 * 60 * 2.4)) / 10),1);
 
 	const sendMessageCount = await MessagingMessages.createQueryBuilder("messaging_message")
 			.where("messaging_message.userId = :userId", { userId: user.id })
@@ -189,13 +190,6 @@ export default define(meta, paramDef, async (ps, me) => {
 	const readMessageCount = await MessagingMessages.createQueryBuilder("messaging_message")
 			.where(" :userId  = ANY(messaging_message.reads) ", { userId: user.id })
 			.getCount();
-			
-	const notesPostDaysMkk = (await Notes.createQueryBuilder("note")
-			.select("count(distinct date_trunc('day',note.\"createdAt\")) count")
-			.where("note.userId = :userId", { userId: user.id })
-			.andWhere("note.visibility <> 'hidden'")
-			.andWhere("'misshaialert' <> ALL(note.tags)")
-			.getRawOne()).count;
 			
 	const result = await awaitAll({
 		notesCount: Notes.createQueryBuilder("note")
@@ -266,6 +260,81 @@ export default define(meta, paramDef, async (ps, me) => {
 			.getRawOne()).count,
 	});
 	
+	const rankResult = await awaitAll({
+		notesCount: Notes.createQueryBuilder("note")
+			.where("note.userId = :userId", { userId: user.id })
+			.addwhere("note.createdAt >= :borderDate", { borderDate: borderDate.toISOString() })
+			.getCount(),
+		repliesCount: Notes.createQueryBuilder("note")
+			.where("note.userId = :userId", { userId: user.id })
+			.andWhere("note.replyId IS NOT NULL")
+			.addwhere("note.createdAt >= :borderDate", { borderDate: borderDate.toISOString() })
+			.getCount(),
+		renotesCount: Notes.createQueryBuilder("note")
+			.where("note.userId = :userId", { userId: user.id })
+			.andWhere("note.renoteId IS NOT NULL")
+			.addwhere("note.createdAt >= :borderDate", { borderDate: borderDate.toISOString() })
+			.getCount(),
+		repliedCount: Notes.createQueryBuilder("note")
+			.where("note.replyUserId = :userId", { userId: user.id })
+			.addwhere("note.createdAt >= :borderDate", { borderDate: borderDate.toISOString() })
+			.getCount(),
+		renotedCount: Notes.createQueryBuilder("note")
+			.where("note.renoteUserId = :userId", { userId: user.id })
+			.addwhere("note.createdAt >= :borderDate", { borderDate: borderDate.toISOString() })
+			.getCount(),
+		pollVotesCount: PollVotes.createQueryBuilder("vote")
+			.where("vote.userId = :userId", { userId: user.id })
+			.addwhere("vote.createdAt >= :borderDate", { borderDate: borderDate.toISOString() })
+			.getCount(),
+		pollVotedCount: PollVotes.createQueryBuilder("vote")
+			.innerJoin("vote.note", "note")
+			.where("note.userId = :userId", { userId: user.id })
+			.addwhere("vote.createdAt >= :borderDate", { borderDate: borderDate.toISOString() })
+			.getCount(),
+		sentReactionsCount: NoteReactions.createQueryBuilder("reaction")
+			.where("reaction.userId = :userId", { userId: user.id })
+			.addwhere("reaction.createdAt >= :borderDate", { borderDate: borderDate.toISOString() })
+			.getCount(),
+		receivedReactionsCount: NoteReactions.createQueryBuilder("reaction")
+			.innerJoin("reaction.note", "note")
+			.where("note.userId = :userId", { userId: user.id })
+			.addwhere("reaction.createdAt >= :borderDate", { borderDate: borderDate.toISOString() })
+			.getCount(),
+		noteFavoritesCount: NoteFavorites.createQueryBuilder("favorite")
+			.where("favorite.userId = :userId", { userId: user.id })
+			.addwhere("favorite.createdAt >= :borderDate", { borderDate: borderDate.toISOString() })
+			.getCount(),
+		pageLikesCount: PageLikes.createQueryBuilder("like")
+			.where("like.userId = :userId", { userId: user.id })
+			.addwhere("like.createdAt >= :borderDate", { borderDate: borderDate.toISOString() })
+			.getCount(),
+		pageLikedCount: PageLikes.createQueryBuilder("like")
+			.innerJoin("like.page", "page")
+			.where("page.userId = :userId", { userId: user.id })
+			.addwhere("like.createdAt >= :borderDate", { borderDate: borderDate.toISOString() })
+			.getCount(),
+		driveFilesCount: DriveFiles.createQueryBuilder("file")
+			.where("file.userId = :userId", { userId: user.id })
+			.addwhere("file.createdAt >= :borderDate", { borderDate: borderDate.toISOString() })
+			.getCount(),
+		notesPostDays: (await Notes.createQueryBuilder("note")
+			.select("count(distinct date_trunc('day',note.\"createdAt\")) count")
+			.where("note.userId = :userId", { userId: user.id })
+			.andWhere("note.visibility <> 'hidden'")
+			.andWhere("'misshaialert' <> ALL(note.tags)")
+			.addwhere("note.createdAt >= :borderDate", { borderDate: borderDate.toISOString() })
+			.getRawOne()).count,
+		sendMessageCount: await MessagingMessages.createQueryBuilder("messaging_message")
+			.where("messaging_message.userId = :userId", { userId: user.id })
+			.addwhere("messaging_message.createdAt >= :borderDate", { borderDate: borderDate.toISOString() })
+			.getCount(),
+		readMessageCount: await MessagingMessages.createQueryBuilder("messaging_message")
+			.where(" :userId  = ANY(messaging_message.reads) ", { userId: user.id })
+			.addwhere("messaging_message.createdAt >= :borderDate", { borderDate: borderDate.toISOString() })
+			.getCount(),
+	});
+	
 	result.followingCount =
 		user.host ? user.followingCount : result.localFollowingCount + result.remoteFollowingCount;
 	result.followersCount =
@@ -291,36 +360,36 @@ export default define(meta, paramDef, async (ps, me) => {
 		result.followingCount * 0.0005 +
 		result.followersCount * 0.0015));
 		
-	const powerMkk = 
-		Math.floor((notesPostDaysMkk * 482 +
-		result.notesCount * 18 +
-		result.repliesCount * 7 +
-		result.renotesCount * -11 +
-		result.repliedCount * 3 +
-		result.renotedCount * 3 +
-		result.pollVotesCount * 7 +
-		result.pollVotedCount * 3 +
-		result.pageLikesCount * 33 +
-		result.pageLikedCount * 27 +
-		result.sentReactionsCount * 7 +
-		result.receivedReactionsCount * 3 +
-		result.driveFilesCount * 6 + 
-		sendMessageCount * 11 +
-		readMessageCount * 2
+	const rankPower = 
+		Math.floor((rankResult.notesPostDays * 482 +
+		rankResult.notesCount * 18 +
+		rankResult.repliesCount * 7 +
+		rankResult.renotesCount * -11 +
+		rankResult.repliedCount * 3 +
+		rankResult.renotedCount * 3 +
+		rankResult.pollVotesCount * 7 +
+		rankResult.pollVotedCount * 3 +
+		rankResult.pageLikesCount * 33 +
+		rankResult.pageLikedCount * 27 +
+		rankResult.sentReactionsCount * 7 +
+		rankResult.receivedReactionsCount * 3 +
+		rankResult.driveFilesCount * 6 + 
+		rankResult.sendMessageCount * 11 +
+		rankResult.readMessageCount * 2
 		) / elapsedDays);
 		
 	const rankBorder = [50,200,400,550,700,850,1000,1500,2000,3000,4000,5000];
 	const rankName = ["G","F","E","D","C","B","B+","A","A+","AA","AA+","AAA","AAA+"];
 	const suffixIncBorder = rankBorder.slice(-1)[0] - rankBorder.slice(-2)[0];
 	
-	if (powerMkk >= rankBorder.slice(-1)[0] + suffixIncBorder) {
-		result.powerRank = rankName.slice(-1)[0] + Math.floor((powerMkk - rankBorder.slice(-2)[0]) / suffixIncBorder);
-	    result.nextRank = Math.floor((powerMkk % suffixIncBorder) / suffixIncBorder * 100) + "%"
+	if (rankPower >= rankBorder.slice(-1)[0] + suffixIncBorder) {
+		result.powerRank = rankName.slice(-1)[0] + Math.floor((rankPower - rankBorder.slice(-2)[0]) / suffixIncBorder);
+	    result.nextRank = Math.floor((rankPower % suffixIncBorder) / suffixIncBorder * 100) + "%"
 	} else {
-		const clearBorder = rankBorder.filter(x => x <= powerMkk);
+		const clearBorder = rankBorder.filter(x => x <= rankPower);
 		result.powerRank = rankName[clearBorder.length];
 		const clearBorderMax = clearBorder.slice(-1)[0] ?? 0
-	    result.nextRank = Math.floor((powerMkk - clearBorderMax) / ((rankBorder[clearBorder.length] ?? (clearBorder.slice(-1)[0] + suffixIncBorder)) - clearBorderMax) * 100) + "%"
+	    result.nextRank = Math.floor((rankPower - clearBorderMax) / ((rankBorder[clearBorder.length] ?? (clearBorder.slice(-1)[0] + suffixIncBorder)) - clearBorderMax) * 100) + "%"
 	}
 
 	return result;

0b72b0d3df996ebdbfcfc765cb5bf5ac0e307a80
emtkmkk <emtk@mkkey.net>
2023-06-16 00:55:17 +0000

    'packages/backend/migration/1682323854723-ShowRenote.js' を更新

--- packages/backend/migration/1682323854723-ShowRenote.js
+++ packages/backend/migration/1682323854723-ShowRenote.js
@@ -1,15 +1,13 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
-
-export class ShowRenote1682323854723 implements MigrationInterface {
+export class ShowRenote1682323854723 {
     name = 'ShowRenote1682323854723'
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
+    async up(queryRunner) {
         await queryRunner.query(`ALTER TABLE "user" ADD "localShowRenote" boolean NOT NULL DEFAULT true`);
         await queryRunner.query(`ALTER TABLE "user" ADD "remoteShowRenote" boolean NOT NULL DEFAULT true`);
         await queryRunner.query(`ALTER TABLE "user" ADD "showSelfRenoteToHome" boolean NOT NULL DEFAULT true`);
     }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
+    async down(queryRunner) {
         await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "localShowRenote"`);
         await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "remoteShowRenote"`);
         await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "showSelfRenoteToHome"`);

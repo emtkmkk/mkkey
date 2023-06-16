fcc6cb43f8fc9186895eceffd940f19f3b0a6f7a
emtkmkk <emtk@mkkey.net>
2023-06-16 01:22:26 +0000

    'packages/backend/migration/1683682889950-addEnumNotification.js' を追加

---
+++ packages/backend/migration/1683682889950-addEnumNotification.js
@@ -0,0 +1,12 @@
+export class addEnumNotification1683682889950 {
+    name = 'addEnumNotification1683682889950'
+
+    async up(queryRunner) {
+        await queryRunner.query(`ALTER TYPE notification_type_enum ADD VALUE IF NOT EXISTS 'unreadAntenna'`);
+        await queryRunner.query(`ALTER TYPE user_profile_mutingnotificationtypes_enum ADD VALUE IF NOT EXISTS 'unreadAntenna'`);
+    }
+
+    async down(queryRunner) {
+    }
+
+}

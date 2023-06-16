eac1acfa943a9241af05befa5f83a3f351930549
emtkmkk <emtk@mkkey.net>
2023-06-16 01:29:09 +0000

    'packages/backend/migration/1682323854723-ShowRenote.js' を更新

--- packages/backend/migration/1682323854723-ShowRenote.js
+++ packages/backend/migration/1682323854723-ShowRenote.js
@@ -2,9 +2,9 @@ export class ShowRenote1682323854723 {
     name = 'ShowRenote1682323854723'
 
     async up(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "user" ADD "localShowRenote" boolean NOT NULL DEFAULT true`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "remoteShowRenote" boolean NOT NULL DEFAULT true`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "showSelfRenoteToHome" boolean NOT NULL DEFAULT true`);
+        await queryRunner.query(`ALTER TABLE "user" ADD IF NOT EXISTS "localShowRenote" boolean NOT NULL DEFAULT true`);
+        await queryRunner.query(`ALTER TABLE "user" ADD IF NOT EXISTS "remoteShowRenote" boolean NOT NULL DEFAULT false`);
+        await queryRunner.query(`ALTER TABLE "user" ADD IF NOT EXISTS "showSelfRenoteToHome" boolean NOT NULL DEFAULT true`);
     }
 
     async down(queryRunner) {

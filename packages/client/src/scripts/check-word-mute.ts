a0cc38f9513bdf91e646e799bbd995855562bd48
emtkmkk <emtk@mkkey.net>
2023-06-23 03:55:21 +0000

    format

--- packages/client/src/scripts/check-word-mute.ts
+++ packages/client/src/scripts/check-word-mute.ts
@@ -27,17 +27,17 @@ function checkWordMute(
 				keywords.every((keyword, index) => {
 					// 反転オプション：- or !
 					const reverse = keyword.startsWith("-") || keyword.startsWith("!");
-					const checkRet = checkMuteKeyword(note,text,keyword.replace(/^\-/,"").replace(/^\!/,""),index,what ?? "note");
+					const checkRet = checkMuteKeyword(note, text, keyword.replace(/^\-/, "").replace(/^\!/, ""), index, what ?? "note");
 					return reverse
 						// エラーならば反転に関係なくfalse
 						? checkRet != null ? !checkRet : false
 						: checkRet ?? false
-					})
+				})
 			) {
 				result.muted = true;
 				let pname = keywords;
 				if (keywords[0].startsWith("pname:")) {
-					pname = [keywords[0].replace("pname:","").replace(/^hidden$/,"").replace("<#id>",note.id)];
+					pname = [keywords[0].replace("pname:", "").replace(/^hidden$/, "").replace("<#id>", note.id)];
 				}
 				result.matched.push(...pname);
 			}
@@ -75,7 +75,7 @@ function checkMuteKeyword(
 ): boolean | undefined {
 	// undefined : エラーの場合 反転を無視してfalse
 	if (!keyword.includes(":") || keyword.startsWith("include:")) {
-		const includeKeyword = keyword.replace("include:","");
+		const includeKeyword = keyword.replace("include:", "");
 		return text.includes(includeKeyword);
 	}
 	if (keyword.startsWith("pname:")) {
@@ -83,76 +83,76 @@ function checkMuteKeyword(
 		return true;
 	}
 	if (keyword.startsWith("exclude:")) {
-		const excludeKeyword = keyword.replace("exclude:","");
+		const excludeKeyword = keyword.replace("exclude:", "");
 		return !text.includes(excludeKeyword);
 	}
 	if (keyword.startsWith("or:")) {
-		const orKeywords = keyword.replace("or:","").split(",");
+		const orKeywords = keyword.replace("or:", "").split(",");
 		if (orKeywords.length > 12) return undefined;
 		return orKeywords.some((orKeyword) => text.includes(orKeyword));
 	}
 	if (keyword.startsWith("fuzzy:")) {
-		const fuzzyKeywords = keyword.replace("fuzzy:","").split("");
+		const fuzzyKeywords = keyword.replace("fuzzy:", "").split("");
 		if (fuzzyKeywords.length > 12) return undefined;
 		return fuzzyKeywords.every((fuzzyKeyword) => text.includes(fuzzyKeyword));
 	}
 	if (keyword.startsWith("from:")) {
-		const fromKeyword = keyword.replace("from:","").replace("@mkkey.net","");
+		const fromKeyword = keyword.replace("from:", "").replace("@mkkey.net", "");
 		return !note.user ? undefined : note.user.host ? note.user.username + "@" + note.user.host === fromKeyword : note.user.username === fromKeyword;
 	}
 	if (keyword.startsWith("host:")) {
-		const hostKeyword = keyword.replace("host:","") || "mkkey.net";
+		const hostKeyword = keyword.replace("host:", "") || "mkkey.net";
 		return !note.user ? undefined : hostKeyword === "mkkey.net" ? !note.user.host : note.user.host === hostKeyword;
 	}
 	if (keyword.startsWith("fuzzyHost:")) {
-		const hostKeyword = keyword.replace("host:","") || "mkkey.net";
+		const hostKeyword = keyword.replace("host:", "") || "mkkey.net";
 		return !note.user ? undefined : ("mkkey.net".includes(hostKeyword) && !note.user.host) || note.user.host.includes(hostKeyword);
 	}
 	if (keyword.startsWith("username:")) {
-		const usernameKeyword = keyword.replace("username:","");
+		const usernameKeyword = keyword.replace("username:", "");
 		return !note.user ? undefined : note.user.username === usernameKeyword;
 	}
 	if (keyword.startsWith("fuzzyUsername:")) {
-		const usernameKeyword = keyword.replace("username:","");
+		const usernameKeyword = keyword.replace("username:", "");
 		return !note.user ? undefined : note.user.username.includes(usernameKeyword);
 	}
 	if (keyword.startsWith("name:")) {
-		const nameKeyword = keyword.replace("name:","");
+		const nameKeyword = keyword.replace("name:", "");
 		return !note.user ? undefined : note.user.name === nameKeyword;
 	}
 	if (keyword.startsWith("fuzzyName:")) {
-		const nameKeyword = keyword.replace("name:","");
+		const nameKeyword = keyword.replace("name:", "");
 		return !note.user ? undefined : note.user.name.includes(nameKeyword);
 	}
 	if (keyword.startsWith("visibility:")) {
-		const visibilityKeyword = keyword.replace("visibility:","");
-		if (visibilityKeyword === "visitor") return ["public","home","hidden"].includes(note.visibility) && note.localOnly === false;
-		if (visibilityKeyword === "private") return ["followers","specified"].includes(note.visibility);
+		const visibilityKeyword = keyword.replace("visibility:", "");
+		if (visibilityKeyword === "visitor") return ["public", "home", "hidden"].includes(note.visibility) && note.localOnly === false;
+		if (visibilityKeyword === "private") return ["followers", "specified"].includes(note.visibility);
 		return note.visibility === visibilityKeyword;
 	}
 	if (keyword.startsWith("localOnly:") || keyword.startsWith("localAndFollower:")) {
-		const localOnlyKeyword = keyword.replace("localOnly:","").replace("localAndFollower:","");
-		if (["true","yes","on"].includes(localOnlyKeyword)) return note.localOnly;
-		if (["false","no","off"].includes(localOnlyKeyword)) return !note.localOnly;
+		const localOnlyKeyword = keyword.replace("localOnly:", "").replace("localAndFollower:", "");
+		if (["true", "yes", "on"].includes(localOnlyKeyword)) return note.localOnly;
+		if (["false", "no", "off"].includes(localOnlyKeyword)) return !note.localOnly;
 		return undefined;
 	}
 	if (keyword.startsWith("what:")) {
-		const whatKeyword = keyword.replace("what:","").replace("rt","renote");
+		const whatKeyword = keyword.replace("what:", "").replace("rt", "renote");
 		return what === whatKeyword;
 	}
 	if (keyword.startsWith("relation:")) {
-		const relationKeyword = keyword.replace("relation:","");
+		const relationKeyword = keyword.replace("relation:", "");
 		if (note.user.isFollowing == null) return undefined;
-		if (["follow","following"].includes(relationKeyword)) return note.user.isFollowing;
-		if (["FollowOnly","followOnly","FollowingOnly","followingOnly"].includes(relationKeyword)) return note.user.isFollowing && !note.user.isFollowed;
-		if (["Follower","follower","Followed","followed"].includes(relationKeyword)) return note.user.isFollowed;
-		if (["FollowerOnly","followerOnly","FollowedOnly","followedOnly"].includes(relationKeyword)) return !note.user.isFollowing && note.user.isFollowed;
+		if (["follow", "following"].includes(relationKeyword)) return note.user.isFollowing;
+		if (["FollowOnly", "followOnly", "FollowingOnly", "followingOnly"].includes(relationKeyword)) return note.user.isFollowing && !note.user.isFollowed;
+		if (["Follower", "follower", "Followed", "followed"].includes(relationKeyword)) return note.user.isFollowed;
+		if (["FollowerOnly", "followerOnly", "FollowedOnly", "followedOnly"].includes(relationKeyword)) return !note.user.isFollowing && note.user.isFollowed;
 		if (relationKeyword === "both") return note.user.isFollowed && note.user.isFollowing;
 		if (relationKeyword === "none") return !note.user.isFollowed && !note.user.isFollowing;
 		return undefined;
 	}
 	if (keyword.startsWith("filter:")) {
-		const filterKeyword = keyword.replace("filter:","");
+		const filterKeyword = keyword.replace("filter:", "");
 		if (filterKeyword.includes("mention")) {
 			const isMention = (note.mentions && !note.replyId);
 			return !!isMention;
@@ -169,7 +169,7 @@ function checkMuteKeyword(
 			const isQuote = (note.renoteId && note.text);
 			return !!isQuote;
 		}
-		if (filterKeyword.includes("media") || filterKeyword.includes("file") ) {
+		if (filterKeyword.includes("media") || filterKeyword.includes("file")) {
 			const isMedia = (note.fileIds && note.fileIds.length !== 0);
 			return !!isMedia;
 		}
@@ -205,7 +205,7 @@ export function getWordSoftMute(
 	}
 
 	if (mutedWords.length > 0) {
-		let noteMuted = checkWordMute(note, mutedWords,"note");
+		let noteMuted = checkWordMute(note, mutedWords, "note");
 		if (noteMuted.muted) {
 			noteMuted.what = "note";
 			return noteMuted;
@@ -220,7 +220,7 @@ export function getWordSoftMute(
 		}
 
 		if (note.reply) {
-			let replyMuted = checkWordMute(note.reply, mutedWords , "reply");
+			let replyMuted = checkWordMute(note.reply, mutedWords, "reply");
 			if (replyMuted.muted) {
 				replyMuted.what = "reply";
 				return replyMuted;

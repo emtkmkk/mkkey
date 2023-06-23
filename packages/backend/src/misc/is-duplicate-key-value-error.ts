4a4720f39257ce94401690667480a86cc25ef09b
emtkmkk <emtk@mkkey.net>
2023-06-23 04:49:15 +0000

    コードのプロパティがない場合のみメッセージの先頭で判定する

--- packages/backend/src/misc/is-duplicate-key-value-error.ts
+++ packages/backend/src/misc/is-duplicate-key-value-error.ts
@@ -1,3 +1,5 @@
 export function isDuplicateKeyValueError(e: unknown | Error): boolean {
-	return (e as Error & {code?: string;}).code === "23505";
+	if (typeof e !== "object" || e === null) return false;
+	const _e = (e as Error & {code?: string;}); 
+	return _e.code != null ? _e.code === "23505" : _e.message?.startsWith("duplicate key value");
 }

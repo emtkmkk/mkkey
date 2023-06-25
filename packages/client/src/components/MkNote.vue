3c11a7e824b791b00943a44076537bdd66dfc138
emtkmkk <emtk@mkkey.net>
2023-06-25 08:18:36 +0000

    'packages/client/src/components/MkNote.vue' を更新

--- packages/client/src/components/MkNote.vue
+++ packages/client/src/components/MkNote.vue
@@ -283,6 +283,7 @@ const props = defineProps<{
 	pinned?: boolean;
 	detailedView?: boolean;
 	notification?: boolean;
+	endpoint?: string;
 }>();
 
 const inChannel = inject("inChannel", null);
@@ -339,7 +340,7 @@ let replyNote = $computed(() =>
 const isMyRenote = $i && $i.id === note.userId;
 const showContent = ref(false);
 const isDeleted = ref(false);
-const muted = ref(getWordSoftMute(note, $i, defaultStore.state.mutedWords));
+const muted = ref(getWordSoftMute(note, $i, defaultStore.state.mutedWords, endpoint));
 const translation = ref(null);
 const translating = ref(false);
 const enableEmojiReactions = defaultStore.state.enableEmojiReactions;

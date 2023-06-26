05f3b38b135a14df4a4eafe48fa0bbf3828fbb14
emtkmkk <emtk@mkkey.net>
2023-06-26 08:48:46 +0000

    フォーマット

--- packages/backend/src/server/api/endpoints/users/stats.ts
+++ packages/backend/src/server/api/endpoints/users/stats.ts
@@ -157,7 +157,7 @@ export const meta = {
 				type: "string",
 				optional: false,
 				nullable: false,
-			    description: "powerrrrrrrrrrrrrr",
+				description: "powerrrrrrrrrrrrrr",
 			}
 		},
 	},
@@ -184,27 +184,27 @@ export default define(meta, paramDef, async (ps, me) => {
 	borderDate.setMinutes(0);
 	borderDate.setSeconds(0);
 	borderDate.setMilliseconds(0);
-	
+
 	const firstLocalFollower = user.host ? Date.parse((await Followings.createQueryBuilder("following")
-			.select('min(following.\"createdAt\")',"min")
-			.where("following.followeeId = :userId", { userId: user.id })
-			.andWhere("following.followerHost IS NULL")
-			.getRawOne()).min) : undefined;
-	
+		.select('min(following.\"createdAt\")', "min")
+		.where("following.followeeId = :userId", { userId: user.id })
+		.andWhere("following.followerHost IS NULL")
+		.getRawOne()).min) : undefined;
+
 	const userCreatedAtDate = firstLocalFollower ? firstLocalFollower : Date.parse(user.createdAt);
-	
+
 	if (firstLocalFollower && borderDate.valueOf() < firstLocalFollower) borderDate = new Date(firstLocalFollower);
-	
-	const elapsedDays = Math.max(Math.min(Math.ceil((now.getTime() - userCreatedAtDate) / (1000 * 60 * 60 * 2.4)) / 10,RANK_TARGET_DAYS),1);
+
+	const elapsedDays = Math.max(Math.min(Math.ceil((now.getTime() - userCreatedAtDate) / (1000 * 60 * 60 * 2.4)) / 10, RANK_TARGET_DAYS), 1);
 
 	const sendMessageCount = await MessagingMessages.createQueryBuilder("messaging_message")
-			.where("messaging_message.userId = :userId", { userId: user.id })
-			.getCount();
-			
+		.where("messaging_message.userId = :userId", { userId: user.id })
+		.getCount();
+
 	const readMessageCount = await MessagingMessages.createQueryBuilder("messaging_message")
-			.where(" :userId  = ANY(messaging_message.reads) ", { userId: user.id })
-			.getCount();
-			
+		.where(" :userId  = ANY(messaging_message.reads) ", { userId: user.id })
+		.getCount();
+
 	const result = await awaitAll({
 		notesCount: Notes.createQueryBuilder("note")
 			.where("note.userId = :userId", { userId: user.id })
@@ -273,7 +273,7 @@ export default define(meta, paramDef, async (ps, me) => {
 			.andWhere("'misshaialert' <> ALL(note.tags)")
 			.getRawOne()).count,
 	});
-	
+
 	const rankResult = await awaitAll({
 		notesCount: Notes.createQueryBuilder("note")
 			.where("note.userId = :userId", { userId: user.id })
@@ -348,73 +348,73 @@ export default define(meta, paramDef, async (ps, me) => {
 			.andWhere("messaging_message.createdAt >= :borderDate", { borderDate: borderDate.toISOString() })
 			.getCount(),
 	});
-	
+
 	result.followingCount =
 		user.host ? user.followingCount : result.localFollowingCount + result.remoteFollowingCount;
 	result.followersCount =
 		user.host ? user.followersCount : result.localFollowersCount + result.remoteFollowersCount;
-		
-	result.power = 
+
+	result.power =
 		Math.floor((result.notesPostDays * 482 +
-		result.notesCount * 18 +
-		result.repliesCount * 7 +
-		result.renotesCount * -11 +
-		result.repliedCount * 3 +
-		result.renotedCount * 3 +
-		result.pollVotesCount * 7 +
-		result.pollVotedCount * 3 +
-		result.pageLikesCount * 33 +
-		result.pageLikedCount * 27 +
-		result.sentReactionsCount * 7 +
-		result.receivedReactionsCount * 3 +
-		result.driveFilesCount * 6 + 
-		sendMessageCount * 11 +
-		readMessageCount * 2
-		) * ( 1 + 
-		result.followingCount * 0.0005 +
-		result.followersCount * 0.0015));
-		
-	const rankPower = 
+			result.notesCount * 18 +
+			result.repliesCount * 7 +
+			result.renotesCount * -11 +
+			result.repliedCount * 3 +
+			result.renotedCount * 3 +
+			result.pollVotesCount * 7 +
+			result.pollVotedCount * 3 +
+			result.pageLikesCount * 33 +
+			result.pageLikedCount * 27 +
+			result.sentReactionsCount * 7 +
+			result.receivedReactionsCount * 3 +
+			result.driveFilesCount * 6 +
+			sendMessageCount * 11 +
+			readMessageCount * 2
+		) * (1 +
+			result.followingCount * 0.0005 +
+			result.followersCount * 0.0015));
+
+	const rankPower =
 		Math.floor((rankResult.notesPostDays * 482 +
-		rankResult.notesCount * 18 +
-		rankResult.repliesCount * 7 +
-		rankResult.renotesCount * -11 +
-		rankResult.repliedCount * 3 +
-		rankResult.renotedCount * 3 +
-		rankResult.pollVotesCount * 7 +
-		rankResult.pollVotedCount * 3 +
-		rankResult.pageLikesCount * 33 +
-		rankResult.pageLikedCount * 27 +
-		rankResult.sentReactionsCount * 7 +
-		rankResult.receivedReactionsCount * 3 +
-		rankResult.driveFilesCount * 6 + 
-		rankResult.sendMessageCount * 11 +
-		rankResult.readMessageCount * 2
+			rankResult.notesCount * 18 +
+			rankResult.repliesCount * 7 +
+			rankResult.renotesCount * -11 +
+			rankResult.repliedCount * 3 +
+			rankResult.renotedCount * 3 +
+			rankResult.pollVotesCount * 7 +
+			rankResult.pollVotedCount * 3 +
+			rankResult.pageLikesCount * 33 +
+			rankResult.pageLikedCount * 27 +
+			rankResult.sentReactionsCount * 7 +
+			rankResult.receivedReactionsCount * 3 +
+			rankResult.driveFilesCount * 6 +
+			rankResult.sendMessageCount * 11 +
+			rankResult.readMessageCount * 2
 		) / elapsedDays);
-	
+
 	let _rankPower = rankPower;
-		
+
 	if (elapsedDays < 14) {
-		_rankPower = Math.min(rankPower,4999);
-		if (elapsedDays < 4) _rankPower = Math.min(rankPower,1599);
-		else if (elapsedDays < 7) _rankPower = Math.min(rankPower,2749);
-		else if (elapsedDays < 10) _rankPower = Math.min(rankPower,4249);
+		_rankPower = Math.min(rankPower, 4999);
+		if (elapsedDays < 4) _rankPower = Math.min(rankPower, 1599);
+		else if (elapsedDays < 7) _rankPower = Math.min(rankPower, 2749);
+		else if (elapsedDays < 10) _rankPower = Math.min(rankPower, 4249);
 	}
-		
-	const rankBorder = [50,125,200,300,400,500,600,700,800,1000,1200,1600,2000,2750,3500,4250,5000,6000];
-	const rankName = ["G","F","F+","E","E+","D","D+","C","C+","B","B+","A","A+","AA","AA+","AAA","AAA+","⭐","⭐+"];
+
+	const rankBorder = [50, 125, 200, 300, 400, 500, 600, 700, 800, 1000, 1200, 1600, 2000, 2750, 3500, 4250, 5000, 6000];
+	const rankName = ["G", "F", "F+", "E", "E+", "D", "D+", "C", "C+", "B", "B+", "A", "A+", "AA", "AA+", "AAA", "AAA+", "⭐", "⭐+"];
 	const suffixIncBorder = rankBorder.slice(-1)[0] - rankBorder.slice(-2)[0];
-	
+
 	if (_rankPower >= rankBorder.slice(-1)[0] + suffixIncBorder) {
 		result.powerRank = rankName.slice(-1)[0] + Math.floor((_rankPower - rankBorder.slice(-2)[0]) / suffixIncBorder);
-	    result.nextRank = Math.floor((rankPower % suffixIncBorder) / suffixIncBorder * 100) + "%";
+		result.nextRank = Math.floor((rankPower % suffixIncBorder) / suffixIncBorder * 100) + "%";
 	} else {
 		const clearBorder = rankBorder.filter(x => x <= _rankPower);
 		result.powerRank = rankName[clearBorder.length];
 		const clearBorderMax = clearBorder.slice(-1)[0] ?? 0;
-	    result.nextRank = Math.floor((rankPower - clearBorderMax) / ((rankBorder[clearBorder.length] ?? (clearBorder.slice(-1)[0] + suffixIncBorder)) - clearBorderMax) * 100) + "%";
+		result.nextRank = Math.floor((rankPower - clearBorderMax) / ((rankBorder[clearBorder.length] ?? (clearBorder.slice(-1)[0] + suffixIncBorder)) - clearBorderMax) * 100) + "%";
 	}
-	
+
 	if (!firstLocalFollower && user.host) result.powerRank = result.powerRank + "?";
 
 	return result;

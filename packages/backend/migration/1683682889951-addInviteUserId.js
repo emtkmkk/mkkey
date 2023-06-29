730d092f6a326e978e9a0a2b56013a3f4611c713
emtkmkk <emtk@mkkey.net>
2023-06-29 04:48:23 +0000

    'packages/backend/migration/1683682889951-addInviteUserId.js' を更新

--- packages/backend/migration/1683682889951-addInviteUserId.js
+++ packages/backend/migration/1683682889951-addInviteUserId.js
@@ -3,13 +3,13 @@ export class addInviteUserId1683682889951 {
 
     async up(queryRunner) {
         await queryRunner.query(`ALTER TABLE "user" ADD IF NOT EXISTS "inviteUserId" varchar(10)`);
-        await queryRunner.query(`ALTER TABLE "user_pendings" ADD IF NOT EXISTS "inviteUserId" varchar(10)`);
+        await queryRunner.query(`ALTER TABLE "user_pending" ADD IF NOT EXISTS "inviteUserId" varchar(10)`);
         await queryRunner.query(`ALTER TABLE "registration_ticket" ADD IF NOT EXISTS "inviteUserId" varchar(10)`);
     }
 
     async down(queryRunner) {
         await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "inviteUserId"`);
-        await queryRunner.query(`ALTER TABLE "user_pendings" DROP COLUMN "inviteUserId"`);
+        await queryRunner.query(`ALTER TABLE "user_pending" DROP COLUMN "inviteUserId"`);
         await queryRunner.query(`ALTER TABLE "registration_ticket" DROP COLUMN "inviteUserId"`);
     }
 

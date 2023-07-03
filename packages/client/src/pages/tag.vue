d72e429e46bed2308bb70395233961cd0d649256
emtkmkk <emtk@mkkey.net>
2023-07-03 09:30:52 +0000

    'packages/client/src/pages/tag.vue' を更新

--- packages/client/src/pages/tag.vue
+++ packages/client/src/pages/tag.vue
@@ -78,7 +78,6 @@ const headerActions = $computed(() => [{
 		title: i18n.ts.postForm,
 		text: i18n.ts.postForm,
 		iconOnly: true,
-		handler: os.post({initialText: `#${props.tag}`}),
 	}]);
 
 const headerTabs = $computed(() => [

22fbd786cf942544b76b6157d0c0e057de841104
emtkmkk <emtk@mkkey.net>
2023-07-06 07:02:50 +0000

    リアクションの名前表示にusername表示と短縮表示を追加

--- packages/client/src/components/MkReactionsViewer.details.vue
+++ packages/client/src/components/MkReactionsViewer.details.vue
@@ -18,7 +18,8 @@
 			<div class="users">
 				<div v-for="u in users" :key="u.id" class="user">
 					<MkAvatar class="avatar" :user="u" />
-					<MkUserName class="name" :user="u" :nowrap="true" />
+					<MkUserName v-if="!$store.state.reactionShowUsername" class="name" :user="u" :max-length="$store.state.reactionShowShort ? 8 : 0" :nowrap="true" />
+					<MkAcct v-if="$store.state.reactionShowUsername" class="name" :user="u" :detail="!$store.state.reactionShowShort" :nowrap="true" />
 				</div>
 				<div v-if="users.length > 10" class="omitted">
 					+{{ count - 10 }}

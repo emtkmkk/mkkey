916e0c43e38a3b3b85d1c10dd43365e02478213c
emtkmkk <emtk@mkkey.net>
2023-07-10 10:40:45 +0000

    'packages/backend/src/server/api/endpoints/meta.ts' を更新

--- packages/backend/src/server/api/endpoints/meta.ts
+++ packages/backend/src/server/api/endpoints/meta.ts
@@ -443,7 +443,7 @@ export default define(meta, paramDef, async (ps, me) => {
 			id: "meta_plus_emojis",
 			milliseconds: 3600000, // 1 hour
 		},
-	})).filter((x) => !emojiNames.includes(x.name) && x.name?.length < 50 && x.url?.length < 120).slice(0,10000)
+	})).filter((x) => !emojiNames.includes(x.name) && (x.name?.length ?? 0) < 50 && (x.publicUrl?.length ?? 0) < 120).slice(0,10000)
 	: undefined;
 
 	const allEmojis = ps.allEmojis 
@@ -458,7 +458,7 @@ export default define(meta, paramDef, async (ps, me) => {
 			id: "meta_all_emojis",
 			milliseconds: 3600000, // 1 hour
 		},
-	})).filter((x) => !emojiNames.includes(x.name) && x.name?.length < 50 && x.host?.length < 35 && x.url?.length < 120)
+	})).filter((x) => !emojiNames.includes(x.name) && (x.name?.length ?? 0) < 50 && (x.host?.length ?? 0) < 35 && (x.publicUrl?.length ?? 0) < 120
 	: undefined;
 
 	const ads = await Ads.find({

9252a5ab87b246ac53e641dc7482197573eb04b8
emtkmkk <emtk@mkkey.net>
2023-07-10 09:02:22 +0000

    'packages/client/src/pages/settings/account-info.vue' を更新

--- packages/client/src/pages/settings/account-info.vue
+++ packages/client/src/pages/settings/account-info.vue
@@ -177,7 +177,7 @@
 			<MkKeyValue oneline style="margin: 1em 0">
 				<template #key>最終取得</template>
 				<template v-if="instance.emojiFetchDate" #value>
-					<MkTime :time="instance.emojiFetchDate" mode="relative"/>{{ "  " + instance.emojiFetchDate }}
+					<MkTime :time="instance.emojiFetchDate" mode="relative"/>
 				</template>
 				<template v-else #value>
 					{{ "N/A" }}

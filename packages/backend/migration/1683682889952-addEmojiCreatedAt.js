67e4fb11572cb8438bd7180b4d0e92584334cefa
emtkmkk <emtk@mkkey.net>
2023-07-14 04:52:01 +0000

    'packages/backend/migration/1683682889952-addEmojiCreatedAt.js' を更新

--- packages/backend/migration/1683682889952-addEmojiCreatedAt.js
+++ packages/backend/migration/1683682889952-addEmojiCreatedAt.js
@@ -3,6 +3,7 @@ export class addEmojiCreatedAt1683682889952 {
 
     async up(queryRunner) {
         await queryRunner.query(`ALTER TABLE "emoji" ADD IF NOT EXISTS "createdAt" TIMESTAMP WITH TIME ZONE`);
+        await queryRunner.query(`UPDATE "emoji" SET "createdAt" = "updatedAt"`);
     }
 
     async down(queryRunner) {

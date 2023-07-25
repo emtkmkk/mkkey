301a1ae06055b28c0e5004265d0b2e8fa258055f
emtkmkk <emtk@mkkey.net>
2023-07-25 14:51:19 +0900

    スレッドミュートしているノートはRTを表示しない

--- packages/backend/src/server/api/common/generate-muted-note-query.ts
+++ packages/backend/src/server/api/common/generate-muted-note-query.ts
@@ -1,5 +1,5 @@
 import type { User } from "@/models/entities/user.js";
-import { MutedNotes } from "@/models/index.js";
+import { MutedNotes, NoteThreadMutings } from "@/models/index.js";
 import type { SelectQueryBuilder } from "typeorm";
 
 export function generateMutedNoteQuery(
@@ -9,8 +9,16 @@ export function generateMutedNoteQuery(
 	const mutedQuery = MutedNotes.createQueryBuilder("muted")
 		.select("muted.noteId")
 		.where("muted.userId = :userId", { userId: me.id });
+		
+	const threadMutedQuery = NoteThreadMutings.createQueryBuilder("threadMuted")
+	.select("threadMuted.threadId")
+	.where("threadMuted.userId = :userId", { userId: me.id });
 
 	q.andWhere(`note.id NOT IN (${mutedQuery.getQuery()})`);
 
 	q.setParameters(mutedQuery.getParameters());
+
+	q.andWhere(`renote.id NOT IN (${threadMutedQuery.getQuery()})`);
+	
+	q.setParameters(threadMutedQuery.getParameters());
 }

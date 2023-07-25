b10dd75bae6523255f966148abaf6c3285b85347
emtkmkk <emtk@mkkey.net>
2023-07-25 12:20:34 +0900

    'packages/client/src/ui/universal.vue' を更新

--- packages/client/src/ui/universal.vue
+++ packages/client/src/ui/universal.vue
@@ -238,7 +238,7 @@ function updateButtonState(): void {
 		buttonAnimIndex.value = 1;
 		return;
 	}
-	if (tbitem ? routerState.includes(navbarItemDef[tbitem].to) : false) {
+	if (tbitem?.to ? routerState.includes(navbarItemDef[tbitem].to) : false) {
 		buttonAnimIndex.value = 2;
 		return;
 	}

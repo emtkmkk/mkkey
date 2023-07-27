30aeea6142a0fcb2f286af9f5dd82b0d665f0e3e
emtkmkk <emtk@mkkey.net>
2023-07-27 14:45:55 +0900

    'packages/backend/migration/addmaxReactionsPerAccount1683682889955.js' を追加

---
+++ packages/backend/migration/addmaxReactionsPerAccount1683682889955.js
@@ -0,0 +1,12 @@
+export class addmaxReactionsPerAccount1683682889955 {
+    name = 'addmaxReactionsPerAccount1683682889955'
+
+    async up(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "instance" ADD IF NOT EXISTS "maxReactionsPerAccount" integer NOT NULL DEFAULT 1`);
+    }
+
+    async down(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "instance" DROP COLUMN "maxReactionsPerAccount"`);
+    }
+
+}

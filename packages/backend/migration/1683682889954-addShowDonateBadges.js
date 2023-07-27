498b33ee49f2a30ea4abc57413fb424bbdc5141e
emtkmkk <emtk@mkkey.net>
2023-07-27 12:15:21 +0900

    'packages/backend/migration/1683682889954-addShowDonateBadges.js' を更新

--- packages/backend/migration/1683682889954-addShowDonateBadges.js
+++ packages/backend/migration/1683682889954-addShowDonateBadges.js
@@ -2,11 +2,11 @@ export class addShowDonateBadges1683682889954 {
     name = 'addShowDonateBadges1683682889954'
 
     async up(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "user" ADD IF NOT EXISTS "showDonateBadges" boolean NOT NULL DEFAULT false`);
+        await queryRunner.query(`ALTER TABLE "user_profile" ADD IF NOT EXISTS "showDonateBadges" boolean NOT NULL DEFAULT false`);
     }
 
     async down(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "showDonateBadges"`);
+        await queryRunner.query(`ALTER TABLE "user_profile" DROP COLUMN "showDonateBadges"`);
     }
 
 }

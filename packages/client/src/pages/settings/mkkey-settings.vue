6765873f785ca7371ae2a6e6904930cb349ef62f
emtkmkk <emtk@mkkey.net>
2023-07-31 17:18:50 +0900

    未設定ラベルを表示しない設定を追加

--- packages/client/src/pages/settings/mkkey-settings.vue
+++ packages/client/src/pages/settings/mkkey-settings.vue
@@ -6,9 +6,16 @@
 		>
 			{{ i18n.ts.notSetOnly }}
 		</FormSwitch>
+		<FormSwitch
+			v-if="!notSetOnly"
+			v-model="dontShowNotSet"
+			class="_formBlock"
+		>
+			{{ i18n.ts.dontShowNotSet }}
+		</FormSwitch>
 		<template v-for="item in items">
 			<FormLink :key="item.key" v-if="!notSetOnly || defaultStore.isDefault(item.key)" :to="`/settings/${item.def.page}`" style="overflow: hidden;text-overflow: ellipsis;" class="_formBlock">
-				<span v-if="!notSetOnly && defaultStore.isDefault(item.key)" class="_beta">{{ i18n.ts.notSet }}</span>{{ i18n.ts[item.key] }}
+				<span v-if="!notSetOnly && !dontShowNotSet && defaultStore.isDefault(item.key)" class="_beta">{{ i18n.ts.notSet }}</span>{{ i18n.ts[item.key] }}
 				<template #suffix><MkTime :time="new Date(item.def.createdAt)" mode="relative" dateOnly /></template>
 			</FormLink>
 		</template>
@@ -27,7 +34,10 @@ import { $i } from "@/account";
 import { i18n } from "@/i18n";
 import { definePageMetadata } from "@/scripts/page-metadata";
 
-const notSetOnly = ref(items.some(x => defaultStore.isDefault(x.key)));
+const notSetOnly = ref(!dontShowNotSet && items.some(x => defaultStore.isDefault(x.key)));
+const dontShowNotSet = computed(
+	defaultStore.makeGetterSetter("dontShowNotSet")
+);
 
 const items = computed(() => {
 	return Object.keys(defaultStore.def)

0f2f48537b5f3410faff374bcd4e9a81a399686f
emtkmkk <emtk@mkkey.net>
2023-08-02 08:00:36 +0900

    'packages/backend/src/server/proxy/proxy-media.ts' を更新

--- packages/backend/src/server/proxy/proxy-media.ts
+++ packages/backend/src/server/proxy/proxy-media.ts
@@ -27,6 +27,18 @@ export async function proxyMedia(ctx: Koa.Context) {
 
 		const { mime, ext } = await detectType(path);
 		const isConvertibleImage = isMimeImage(mime, "sharp-convertible-image");
+		
+		if (
+            'static' in ctx.query ||
+            'preview' in ctx.query ||
+            'badge' in ctx.query
+        ) {
+            if (!isConvertibleImage) {
+                // 画像でないなら404でお茶を濁す
+                throw new StatusError('Unexpected mime', 404);
+            }
+        }
+
 
 		let image: IImage;
 

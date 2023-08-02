b6a778fc19808ab45a277010c2cc66a012046d05
emtkmkk <emtk@mkkey.net>
2023-08-02 13:32:55 +0900

    self.closeが定義されている場合のみself.closeを実行

--- packages/client/src/directives/tooltip.ts
+++ packages/client/src/directives/tooltip.ts
@@ -113,6 +113,8 @@ export default {
 	unmounted(el, binding, vn) {
 		const self = el._tooltipDirective_;
 		window.clearInterval(self.checkTimer);
-		self.close();
+		if (self.close) { // Check if self.close is defined
+			self.close();
+		}
 	},
 } as Directive;

ba861735a0b8ccbed20d3f923cd6ccc872fdf9a2
emtkmkk <emtk@mkkey.net>
2023-08-06 22:29:27 +0900

    'packages/backend/src/server/api/index.ts' を更新

--- packages/backend/src/server/api/index.ts
+++ packages/backend/src/server/api/index.ts
@@ -84,7 +84,7 @@ mastoRouter.use(
 
 mastoFileRouter.post("/v1/media", upload.single("file"), async (ctx) => {
 	const BASE_URL = `${ctx.protocol}://${ctx.hostname}`;
-	const accessTokens = ctx.request?.body?.i || ctx.headers.authorization;
+	const accessTokens = ctx.request?.body?.i?.trim() || ctx.headers.authorization;
 	const client = getClient(BASE_URL, accessTokens);
 	try {
 		let multipartData = await ctx.file;
@@ -103,7 +103,7 @@ mastoFileRouter.post("/v1/media", upload.single("file"), async (ctx) => {
 });
 mastoFileRouter.post("/v2/media", upload.single("file"), async (ctx) => {
 	const BASE_URL = `${ctx.protocol}://${ctx.hostname}`;
-	const accessTokens = ctx.headers.authorization;
+	const accessTokens = ctx.request?.body?.i?.trim() || ctx.headers.authorization;
 	const client = getClient(BASE_URL, accessTokens);
 	try {
 		let multipartData = await ctx.file;

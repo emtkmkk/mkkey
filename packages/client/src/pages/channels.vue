ab0f38fd8b7319e69c917d3eb0be01fa7dbac63d
emtkmkk <emtk@mkkey.net>
2023-08-07 16:13:15 +0900

    'packages/client/src/pages/channels.vue' を更新

--- packages/client/src/pages/channels.vue
+++ packages/client/src/pages/channels.vue
@@ -8,12 +8,14 @@
 		/></template>
 		<MkSpacer :content-max="700">
 			<swiper
+				:touch-angle="$store.state.swipeTouchAngle"
+				:threshold="$store.state.swipeThreshold"
+				:centeredSlides="$store.state.swipeCenteredSlides"
 				:modules="[Virtual]"
 				:space-between="20"
 				:virtual="true"
 				:allow-touch-move="
 					!(
-						deviceKind === 'desktop' &&
 						!defaultStore.state.swipeOnDesktop
 					)
 				"

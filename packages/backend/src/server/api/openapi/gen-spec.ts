9437fe75412c9902c14165424bbc11e30c3b5f61
emtkmkk <emtk@mkkey.net>
2023-08-07 10:02:11 +0900

    'packages/backend/src/server/api/openapi/gen-spec.ts' を更新

--- packages/backend/src/server/api/openapi/gen-spec.ts
+++ packages/backend/src/server/api/openapi/gen-spec.ts
@@ -15,7 +15,7 @@ export function genOpenapiSpec() {
 
 		externalDocs: {
 			description: "Repository",
-			url: "https://codeberg.org/calckey/calckey",
+			url: "https://code.naskya.net/emtkmkk/mkkey",
 		},
 
 		servers: [
@@ -106,7 +106,7 @@ export function genOpenapiSpec() {
 			description: desc,
 			externalDocs: {
 				description: "Source code",
-				url: `https://codeberg.org/calckey/calckey/src/branch/develop/packages/backend/src/server/api/endpoints/${endpoint.name}.ts`,
+				url: `https://code.naskya.net/emtkmkk/mkkey/src/branch/beta/packages/backend/src/server/api/endpoints/${endpoint.name}.ts`,
 			},
 			tags: endpoint.meta.tags || undefined,
 			security,

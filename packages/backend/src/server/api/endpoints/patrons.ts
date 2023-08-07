7b92567bbb9b5ef696cc9b85145865963c3fcac0
emtkmkk <emtk@mkkey.net>
2023-08-07 10:16:30 +0900

    'packages/backend/src/server/api/endpoints/patrons.ts' を更新

--- packages/backend/src/server/api/endpoints/patrons.ts
+++ packages/backend/src/server/api/endpoints/patrons.ts
@@ -1,4 +1,5 @@
 import define from "../define.js";
+import { redisClient } from "@/db/redis.js";
 
 export const meta = {
 	tags: ["meta"],
@@ -14,15 +15,21 @@ export const paramDef = {
 	required: [],
 } as const;
 
-export default define(meta, paramDef, async () => {
+export default define(meta, paramDef, async (ps) => {
 	let patrons;
-	await fetch(
-		"https://git.joinfirefish.org/firefish/firefish/-/raw/develop/patrons.json",
-	)
-		.then((response) => response.json())
-		.then((data) => {
-			patrons = data["patrons"];
-		});
-
-	return patrons;
+	const cachedPatrons = await redisClient.get("patrons");
+	if (!ps.forceUpdate && cachedPatrons) {
+		patrons = JSON.parse(cachedPatrons);
+	} else {
+		
+		patrons = await fetch(
+			"https://git.joinfirefish.org/firefish/firefish/-/raw/develop/patrons.json",
+		)
+			.then((response) => response.json())
+		await redisClient.set("patrons", JSON.stringify(patrons), "EX", 3600);
+	}
+	return {
+		patrons: patrons["patrons"],
+		sponsors: patrons["sponsors"],
+	};
 });

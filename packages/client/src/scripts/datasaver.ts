ea09607ef10d0eef9edf82374ccec7ce3981cdf3
emtkmkk <emtk@mkkey.net>
2023-08-15 16:34:34 +0900

    'packages/client/src/scripts/datasaver.ts' を追加

---
+++ packages/client/src/scripts/datasaver.ts
@@ -0,0 +1,26 @@
+import { defaultStore } from '@/store';
+
+function isEnableDataSaver(type: string): boolean {
+	return ['cellular', 'unknown'].includes(type);
+}
+
+export function isSupportNavigatorConnection(): boolean {
+	const connection = (navigator as any).connection;
+	return connection && connection.type && ('onchange' in connection);
+}
+
+export function isMobileData(): boolean {
+	const connection = (navigator as any).connection;
+	if (!isSupportNavigatorConnection()) return false;
+	return isEnableDataSaver(connection.type);
+}
+
+export function initializeDetectNetworkChange(): void {
+	const connection = (navigator as any).connection;
+	if (!isSupportNavigatorConnection()) return;
+
+	connection.addEventListener('change', () => {
+		if (!connection || !connection.type) return;
+		defaultStore.set('enableDataSaverMode', isEnableDataSaver(connection.type));
+	});
+}

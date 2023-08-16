b0457ead1416e66c9a727f3b3fc9bc8b7d27e425
emtkmkk <emtk@mkkey.net>
2023-08-16 12:32:29 +0900

    'packages/client/src/components/global/MkUserName.vue' を更新

--- packages/client/src/components/global/MkUserName.vue
+++ packages/client/src/components/global/MkUserName.vue
@@ -7,23 +7,39 @@
 		:author="user"
 		:custom-emojis="user.emojis"
 	/>
+	<img 
+		class="icon"
+		v-if="hostIcon && getIcon(hostIcon) && !errorIcon"
+		:src="getIcon(hostIcon)"
+		aria-hidden="true"
+		@error="errorIcon = true"
+	/>
+	{{ hostIcon && errorIcon && user.host ? '@' + user.host : ''}}
 </template>
 
 <script lang="ts" setup>
 import {} from "vue";
 import * as misskey from "calckey-js";
+import { getProxiedImageUrlNullable } from "@/scripts/media-proxy";
 
 const props = withDefaults(
 	defineProps<{
 		user: misskey.entities.User;
 		nowrap?: boolean;
 		maxlength?: number;
+		hosticon?: string;
 	}>(),
 	{
 		nowrap: true,
 		maxlength: 0,
 	}
 );
+let errorIcon = $ref(false);
+function getIcon(url): string {
+		return (
+					getProxiedImageUrlNullable(url, "preview")
+		);
+}
 </script>
 
 <style lang="scss" module>

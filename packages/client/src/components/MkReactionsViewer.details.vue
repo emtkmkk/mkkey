915b24cef9a26c4feee87a8623ad0d2091870db9
emtkmkk <emtk@mkkey.net>
2023-08-16 13:02:16 +0900

    'packages/client/src/components/MkReactionsViewer.details.vue' を更新

--- packages/client/src/components/MkReactionsViewer.details.vue
+++ packages/client/src/components/MkReactionsViewer.details.vue
@@ -19,7 +19,7 @@
 			<div class="users">
 				<div v-for="u in users" :key="u.id" class="user">
 					<MkAvatar class="avatar" :user="u" />
-					<MkUserName v-if="!$store.state.reactionShowUsername" class="name" :user="u" :maxlength="$store.state.reactionShowShort ? 8 : 0" :nowrap="true" />
+					<MkUserName v-if="!$store.state.reactionShowUsername" class="name" :user="u" :maxlength="$store.state.reactionShowShort ? 8 : 0" :nowrap="true" :hostIcon="u.instance?.faviconUrl || u.instance?.iconUrl || u.host"/>
 					{{ !$store.state.reactionShowUsername && u.host ? "@" + ($store.state.reactionShowShort && u.host.length > 8 ? u.host.slice(0,4) + "…" + u.host.slice(-4) : u.host) : "" }}
 					<MkAcct v-if="$store.state.reactionShowUsername" class="name" :user="u" :maxlength="$store.state.reactionShowShort ? 8 : 0" />
 				</div>

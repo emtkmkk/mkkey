35b147009a36ecee1cdaf22f79b4c1065b38b471
emtkmkk <emtk@mkkey.net>
2023-08-18 11:10:23 +0900

    'packages/backend/src/misc/is-include-ng-word.ts' を更新

--- packages/backend/src/misc/is-include-ng-word.ts
+++ packages/backend/src/misc/is-include-ng-word.ts
@@ -4,7 +4,7 @@ export function isIncludeNgWord(note: any): string {
 		return "";
 	}
 	
-	const ngword = ["おなほ","おっぱい","ぱいおつ","ぱいずり","乳首","ちくび","射精","しゃせい","おなに","精液","せいえき","ちんちん","ちんぽ","ちんこ","うんこ","うんち","おしっこ","まんこ","ふたなり","れいぷ","せっくす","せくーす","ヴぁぎな","しこっ","性器","処女","受精","自慰","勃起"]
+	const ngword = ["おなほ","おっぱい","ぱいおつ","ぱいずり","乳首","ちくび","射精","しゃせい","おなに","精液","せいえき","ちんちん","ちんぽ","ちんこ","うんこ","うんち","おしっこ","まんこ","ふたなり","れいぷ","せっくす","せくーす","ヴぁぎな","しこっ","性器","処女","受精","自慰","勃起"];
 	
 	const text = note.text
 		.replaceAll(/\s/g,"")
@@ -12,7 +12,7 @@ export function isIncludeNgWord(note: any): string {
 		.replace(/[ァ-ン]/g, function (match) {
 			var chr = match.charCodeAt(0) - 0x60;
 			return String.fromCharCode(chr);
-		})
+		});
 	
 	if (ngword.some((x) => {
 		text.includes(x);

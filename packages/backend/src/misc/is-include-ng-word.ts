d63c99f284755f73b7b319f236592901d2e4a5e4
emtkmkk <emtk@mkkey.net>
2023-08-18 11:04:58 +0900

    'packages/backend/src/misc/is-include-ng-word.ts' を更新

--- packages/backend/src/misc/is-include-ng-word.ts
+++ packages/backend/src/misc/is-include-ng-word.ts
@@ -7,8 +7,8 @@ export function isIncludeNgWord(note: any): string {
 	const ngword = ["おなほ","おっぱい","ぱいおつ","ぱいずり","乳首","ちくび","射精","しゃせい","おなに","精液","せいえき","ちんちん","ちんぽ","ちんこ","うんこ","うんち","おしっこ","まんこ","ふたなり","れいぷ","せっくす","せくーす","ヴぁぎな","しこっ","性器","処女","受精","自慰","勃起"]
 	
 	const text = note.text
-		.replaceAll(/\s/,"")
-		.replaceAll(/[!-\/:-@[-`{-~]+/,"")
+		.replaceAll(/\s/g,"")
+		.replaceAll(/[!-\/:-@[-`{-~]+/g,"")
 		.replace(/[ァ-ン]/g, function (match) {
 			var chr = match.charCodeAt(0) - 0x60;
 			return String.fromCharCode(chr);

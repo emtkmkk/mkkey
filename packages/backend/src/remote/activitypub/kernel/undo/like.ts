8e22ce7aca036eb373ae432405739bb53562a00b
emtkmkk <emtk@mkkey.net>
2023-08-22 15:03:03 +0900

    'packages/backend/src/remote/activitypub/kernel/undo/like.ts' を更新

--- packages/backend/src/remote/activitypub/kernel/undo/like.ts
+++ packages/backend/src/remote/activitypub/kernel/undo/like.ts
@@ -2,7 +2,7 @@ import type { CacheableRemoteUser } from "@/models/entities/user.js";
 import type { ILike } from "../../type.js";
 import { getApId } from "../../type.js";
 import deleteReaction from "@/services/note/reaction/delete.js";
-import { fetchNote } from "../../models/note.js";
+import { fetchNote, extractEmojis } from "../../models/note.js";
 
 /**
  * Process Undo.Like activity
@@ -13,9 +13,18 @@ export default async (actor: CacheableRemoteUser, activity: ILike) => {
 	const note = await fetchNote(targetUri);
 	if (!note) return `skip: target note not found ${targetUri}`;
 	
-	const emoji = activity.object?.content;
+	const react = activity._misskey_reaction || activity.content || activity.name;
+	const reactName = react?.split("@")?.[0];
+	const reactHost = react?.split("@")?.[1] ?? undefined;
+	
+	const emoji = await extractEmojis(activity.tag || [], actor.host).catch(() => null);
+	const reactEmoji = emoji?.filter((x) => x.name === reactName && (!reactHost || (x.host ?? "mkkey.net") === reactHost));
 
-	await deleteReaction(actor, note, emoji).catch((e) => {
+	await deleteReaction(
+		actor,
+		note,
+		reactName + (reactEmoji?.[0]?.host ? "@" + reactEmoji[0].host : ""),
+	).catch((e) => {
 		if (e.id === "60527ec9-b4cb-4a88-a6bd-32d3ad26817d") return;
 		throw e;
 	});

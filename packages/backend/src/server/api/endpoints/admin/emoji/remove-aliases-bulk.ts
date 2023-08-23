8e0320c5231adfab022b4fadeb58d8c48b67caca
emtkmkk <emtk@mkkey.net>
2023-08-23 10:49:09 +0900

    packages/backend/src/server/api/endpoints/admin/emoji/remove-aliases-bulk.ts を更新

--- packages/backend/src/server/api/endpoints/admin/emoji/remove-aliases-bulk.ts
+++ packages/backend/src/server/api/endpoints/admin/emoji/remove-aliases-bulk.ts
@@ -3,6 +3,7 @@ import { Emojis } from "@/models/index.js";
 import { In } from "typeorm";
 import { ApiError } from "../../../error.js";
 import { db } from "@/db/postgre.js";
+import { publishBroadcastStream } from "@/services/stream.js";
 
 export const meta = {
 	tags: ["admin"],
@@ -42,6 +43,10 @@ export default define(meta, paramDef, async (ps) => {
 			aliases: emoji.aliases.filter((x) => !ps.aliases.includes(x)),
 		});
 	}
+	
+	publishBroadcastStream("emojiUpdated", {
+		emojis: await Emojis.packMany(emojis),
+	});
 
 	await db.queryResultCache!.remove(["meta_emojis"]);
 });

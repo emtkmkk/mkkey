10952422597a0999d5a1ba7455705ebecaf1dddc
emtkmkk <emtk@mkkey.net>
2023-08-26 15:35:19 +0900

    packages/client/src/components/MkPostForm.vue を更新

--- packages/client/src/components/MkPostForm.vue
+++ packages/client/src/components/MkPostForm.vue
@@ -577,7 +577,7 @@ let hasNotSpecifiedMentions = $ref(false);
 let includesOtherServerEmoji = $ref(false);
 let recentHashtags = $ref(JSON.parse(localStorage.getItem("hashtags") || "[]"));
 let canPublic = $ref((!props.reply || props.reply.visibility === "public") && (!props.renote || props.renote.visibility === "public") && (!props.airReply || props.airReply.visibility === "public")  && !$i.blockPostPublic && !$i.isSilenced);
-let canHome = $ref((!props.reply || (props.reply.visibility === "public" || props.reply.visibility === "home")) && (!props.renote || (props.renote.visibility === "public" || props.renote.visibility === "home"))  && (!props.renote || (props.airReply.visibility === "public" || props.airReply.visibility === "home")) && !$i.blockPostHome && !$i.isSilenced);
+let canHome = $ref((!props.reply || (props.reply.visibility === "public" || props.reply.visibility === "home")) && (!props.renote || (props.renote.visibility === "public" || props.renote.visibility === "home"))  && (!props.airReply || (props.airReply.visibility === "public" || props.airReply.visibility === "home")) && !$i.blockPostHome && !$i.isSilenced);
 let canFollower = $ref((!props.reply || props.reply.visibility !== "specified") && (!props.renote || props.renote.visibility !== "specified") && (!props.airReply || props.airReply.visibility !== "specified") );
 let canNotLocal = $ref((!props.reply || !props.reply.localOnly) && (!props.renote || !props.renote.localOnly) && (!props.airReply || props.airReply.host) && !$i.blockPostNotLocal && !props.channel?.description?.includes("[localOnly]") && !$i.isSilenced);
 let requiredFilename = $ref(props.channel?.description?.includes("[requiredFilename]"))

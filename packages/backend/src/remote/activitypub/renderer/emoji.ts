047141c225c48c07f2aacf4fffbd9b13be123d88
emtkmkk <emtk@mkkey.net>
2023-08-30 13:21:07 +0900

    packages/backend/src/remote/activitypub/renderer/emoji.ts を更新

--- packages/backend/src/remote/activitypub/renderer/emoji.ts
+++ packages/backend/src/remote/activitypub/renderer/emoji.ts
@@ -16,10 +16,16 @@ export default (emoji: Emoji) => ({
 		url: emoji.publicUrl || emoji.originalUrl, // || emoji.originalUrl してるのは後方互換性のため
 	},
 	keyword: emoji.aliases,
-	copyPermission: emoji.license?.includes("コピー可否 : ") ? /コピー可否 : (\w+)(,|$)/.exec(emoji.license)?.[1] ?? "none" : "none",
-	license: emoji.license?.includes("ライセンス : ") ? /ライセンス : ([^,]+)(,|$)/.exec(emoji.license)?.[1] ?? null : null,
-	usageInfo: emoji.license?.includes("使用情報 : ") ? /使用情報 : ([^,]+)(,|$)/.exec(emoji.license)?.[1] ?? undefined : undefined,
-	author: emoji.license?.includes("作者 : ") ? /作者 : ([^,]+)(,|$)/.exec(emoji.license)?.[1] ?? undefined : undefined,
-	description: emoji.license?.includes("説明 : ") ? /説明 : ([^,]+)(,|$)/.exec(emoji.license)?.[1] ?? undefined : undefined,
-	isBasedOnUrl: emoji.license?.includes("元画像 : ") ? /元画像 : ([^,]+)(,|$)/.exec(emoji.license)?.[1] ?? undefined : undefined,
+	...(emoji.license === "文字だけ" ? {
+		copyPermission: "allow",
+		license: "CC0 1.0 Universal",
+		author: "mkkey.net"
+	} : {
+		copyPermission: emoji.license?.includes("コピー可否 : ") ? /コピー可否 : (\w+)(,|$)/.exec(emoji.license)?.[1] ?? "none" : "none",
+		license: emoji.license?.includes("ライセンス : ") ? /ライセンス : ([^,]+)(,|$)/.exec(emoji.license)?.[1] ?? null : null,
+		usageInfo: emoji.license?.includes("使用情報 : ") ? /使用情報 : ([^,]+)(,|$)/.exec(emoji.license)?.[1] ?? undefined : undefined,
+		author: emoji.license?.includes("作者 : ") ? /作者 : ([^,]+)(,|$)/.exec(emoji.license)?.[1] ?? undefined : undefined,
+		description: emoji.license?.includes("説明 : ") ? /説明 : ([^,]+)(,|$)/.exec(emoji.license)?.[1] ?? undefined : undefined,
+		isBasedOnUrl: emoji.license?.includes("元画像 : ") ? /元画像 : ([^,]+)(,|$)/.exec(emoji.license)?.[1] ?? undefined : undefined,
+	})
 });

29bd101845257d68e0e35c1541bb094e3b5f9ec8
emtkmkk <emtk@mkkey.net>
2023-08-31 14:23:50 +0900

    packages/client/src/components/global/MkStickyContainer.vue を更新

--- packages/client/src/components/global/MkStickyContainer.vue
+++ packages/client/src/components/global/MkStickyContainer.vue
@@ -6,6 +6,9 @@
 		<div ref="bodyEl" :data-sticky-container-header-height="headerHeight">
 			<slot></slot>
 		</div>
+		<div ref="footerEl">
+			<slot name="footer"></slot>
+		</div>
 	</div>
 </template>
 
@@ -16,20 +19,29 @@ const CURRENT_STICKY_TOP = "CURRENT_STICKY_TOP";
 </script>
 
 <script lang="ts" setup>
-import { onMounted, onUnmounted, provide, inject, Ref, ref, watch } from "vue";
+import { onMounted, onUnmounted, provide, inject, Ref, ref, watch, shallowRef } from "vue";
 
-const rootEl = $ref<HTMLElement>();
-const headerEl = $ref<HTMLElement>();
-const bodyEl = $ref<HTMLElement>();
+const rootEl = $shallowRef<HTMLElement>();
+const headerEl = $shallowRef<HTMLElement>();
+const bodyEl = $shallowRef<HTMLElement>();
+const footerEl = $shallowRef<HTMLElement>();
 
 let headerHeight = $ref<string | undefined>();
 let childStickyTop = $ref(0);
 const parentStickyTop = inject<Ref<number>>(CURRENT_STICKY_TOP, ref(0));
 provide(CURRENT_STICKY_TOP, $$(childStickyTop));
 
+let footerHeight = $ref<string | undefined>();
+let childStickyBottom = $ref(0);
+const parentStickyBottom = inject<Ref<number>>(CURRENT_STICKY_BOTTOM, ref(0));
+provide("CURRENT_STICKY_BOTTOM", $$(childStickyBottom));
+
+
 const calc = () => {
 	childStickyTop = parentStickyTop.value + headerEl.offsetHeight;
 	headerHeight = headerEl.offsetHeight.toString();
+	childStickyBottom = parentStickyBottom.value + footerEl.offsetHeight;
+	footerHeight = footerEl.offsetHeight.toString();
 };
 
 const observer = new ResizeObserver(() => {
@@ -41,7 +53,7 @@ const observer = new ResizeObserver(() => {
 onMounted(() => {
 	calc();
 
-	watch(parentStickyTop, calc);
+	watch([parentStickyTop, parentStickyBottom], calc);
 
 	watch(
 		$$(childStickyTop),
@@ -52,12 +64,27 @@ onMounted(() => {
 			immediate: true,
 		}
 	);
+	watch(
+		$$(childStickyBottom),
+		() => {
+			bodyEl.style.setProperty('--stickyBottom', `${childStickyBottom}px`);
+		},
+		{
+			immediate: true,
+		}
+	);
+
 
 	headerEl.style.position = "sticky";
 	headerEl.style.top = "var(--stickyTop, 0)";
 	headerEl.style.zIndex = "1000";
-
+	
+	footerEl.style.position = 'sticky';
+	footerEl.style.bottom = 'var(--stickyBottom, 0)';
+	footerEl.style.zIndex = '1000';
+	
 	observer.observe(headerEl);
+	observer.observe(footerEl);
 });
 
 onUnmounted(() => {

8ee39d47b65fcf3157d63ec24a76bd03062fa868
emtkmkk <emtk@mkkey.net>
2023-08-31 17:11:56 +0900

    データセーバーなら、ボタンを押すまで画像一覧を読み込まない

--- packages/client/src/pages/user/images.vue
+++ packages/client/src/pages/user/images.vue
@@ -1,14 +1,16 @@
 <template>
 	<MkSpacer :content-max="800">
-		<XPhotos :key="user.id + 'detail'" :user="user" :limit="100"/>
+		<MkButton style="text-align: center; margin: auto; margin-top: calc(var(--margin) / 2);" v-if="!showImages" @click="showImages = true">{{ "画像一覧を読み込む" }}</MkButton>
+		<XPhotos v-if="showImages" :key="user.id + 'detail'" :user="user" :limit="100"/>
 	</MkSpacer>
 </template>
 
 <script lang="ts" setup>
-import { defineAsyncComponent } from "vue";
+import { defineAsyncComponent, ref } from "vue";
 import * as misskey from "calckey-js";
 
 const XPhotos = defineAsyncComponent(() => import("./index.photos.vue"));
+let showImages = ref(!defaultStore.state.enableDataSaverMode);
 
 const props = defineProps<{
 	user: misskey.entities.User;

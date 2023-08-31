5cf062c80f660966b3d2b34d44f4ef8e126f8feb
emtkmkk <emtk@mkkey.net>
2023-08-31 15:52:07 +0900

    packages/client/src/pages/user/images.vue を追加

---
+++ packages/client/src/pages/user/images.vue
@@ -0,0 +1,17 @@
+<template>
+	<MkSpacer :content-max="800">
+		<XPhotos :key="user.id" :user="user" :limit="100"/>
+	</MkSpacer>
+</template>
+
+<script lang="ts" setup>
+import {} from "vue";
+import * as misskey from "calckey-js";
+
+const props = defineProps<{
+	user: misskey.entities.User;
+}>();
+
+</script>
+
+<style lang="scss" scoped></style>

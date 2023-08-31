3d3657f260fa315256fce8d97dfcb2ade00f0e4e
emtkmkk <emtk@mkkey.net>
2023-08-31 09:42:06 +0900

    packages/backend/src/misc/is-duplicate-key-value-error.ts を更新

--- packages/backend/src/misc/is-duplicate-key-value-error.ts
+++ packages/backend/src/misc/is-duplicate-key-value-error.ts
@@ -1,5 +1,5 @@
+import { QueryFailedError } from "typeorm";
+
 export function isDuplicateKeyValueError(e: unknown | Error): boolean {
-	if (typeof e !== "object" || e === null) return false;
-	const _e = (e as Error & {code?: string;}); 
-	return _e.code != null ? _e.code === "23505" : _e.message?.startsWith("duplicate key value");
+	return e instanceof QueryFailedError && e.driverError.code === "23505";
 }

062da401c9c039967343cf0b57a0618685f3c458
emtkmkk <emtk@mkkey.net>
2023-09-04 18:56:22 +0900

    packages/backend/src/server/api/endpoints/charts/hashtag.ts を更新

--- packages/backend/src/server/api/endpoints/charts/hashtag.ts
+++ packages/backend/src/server/api/endpoints/charts/hashtag.ts
@@ -1,12 +1,11 @@
 import { getJsonSchema } from "@/services/chart/core.js";
-import { hashtagChart } from "@/services/chart/index.js";
 import define from "../../define.js";
 
 export const meta = {
 	tags: ["charts", "hashtags"],
 	requireCredentialPrivateMode: true,
 
-	res: getJsonSchema(hashtagChart.schema),
+	res: null,
 
 	allowGet: true,
 	cacheSec: 60 * 60,
@@ -24,10 +23,5 @@ export const paramDef = {
 } as const;
 
 export default define(meta, paramDef, async (ps) => {
-	return await hashtagChart.getChart(
-		ps.span,
-		ps.limit,
-		ps.offset ? new Date(ps.offset) : null,
-		ps.tag,
-	);
+	return null;
 });

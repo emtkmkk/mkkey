1ff371ca511b24be21e6d1b48f1c8d5f3be1553b
emtkmkk <emtk@mkkey.net>
2023-09-06 12:21:46 +0900

    packages/backend/migration/1683682889959-addIsFirstNote.js を追加

---
+++ packages/backend/migration/1683682889959-addIsFirstNote.js
@@ -0,0 +1,12 @@
+export class addIsFirstNote1683682889959 {
+    name = 'addIsFirstNote1683682889959'
+
+    async up(queryRunner) {
+		await queryRunner.query(`ALTER TABLE "note" ADD IF NOT EXISTS "isFirstNote" boolean NOT NULL DEFAULT FALSE`);
+    }
+
+    async down(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "note" DROP COLUMN "isFirstNote"`);
+    }
+
+}

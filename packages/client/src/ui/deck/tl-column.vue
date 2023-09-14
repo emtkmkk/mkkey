345ce2f779e80efec77ed22d0f646c1cf5f353f6
emtkmkk <emtk@mkkey.net>
2023-09-14 13:38:12 +0900

    packages/client/src/ui/deck/tl-column.vue を更新

--- packages/client/src/ui/deck/tl-column.vue
+++ packages/client/src/ui/deck/tl-column.vue
@@ -25,7 +25,7 @@
 				v-else-if="column.tl === 'global'"
 				class="ph-planet ph-bold ph-lg"
 			></i>
-			<span style="margin-left: 8px">{{ column.name + (columnActive && indicated ? ` (${q})` : '') }}</span>
+			<span style="margin-left: 8px">{{ column.name + (columnActive && indicated ? ` (${q >= 29 ? '29+' : q})` : '') }}</span>
 		</template>
 
 		<div v-if="disabled" class="iwaalbte">

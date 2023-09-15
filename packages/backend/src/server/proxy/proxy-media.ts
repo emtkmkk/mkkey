d97c7b1c81b6aeecd8d61116ebf2585d373730de
emtkmkk <emtk@mkkey.net>
2023-09-15 14:52:16 +0900

    packages/backend/src/server/proxy/proxy-media.ts を更新

--- packages/backend/src/server/proxy/proxy-media.ts
+++ packages/backend/src/server/proxy/proxy-media.ts
@@ -1,4 +1,6 @@
 import * as fs from "node:fs";
+import net from "node:net";
+import { promises } from "node:dns";
 import type Koa from "koa";
 import sharp from "sharp";
 import type { IImage } from "@/services/drive/image-processor.js";
@@ -12,10 +14,46 @@ import { serverLogger } from "../index.js";
 import { isMimeImage } from "@/misc/is-mime-image.js";
 
 export async function proxyMedia(ctx: Koa.Context) {
-	const url = "url" in ctx.query ? ctx.query.url : `https://${ctx.params.url}`;
+	let url = "url" in ctx.query ? ctx.query.url : `https://${ctx.params.url}`;
 
 	if (typeof url !== "string") {
 		ctx.status = 400;
+		return;
+	}
+
+	url = url.replace("//", "/");
+
+	const { hostname } = new URL(url);
+	let resolvedIps;
+	try {
+		resolvedIps = await promises.resolve(hostname);
+	} catch (error) {
+		ctx.status = 400;
+		ctx.body = { message: "Invalid URL" };
+		return;
+	}
+
+	const isSSRF = resolvedIps.some((ip) => {
+		if (net.isIPv4(ip)) {
+			const parts = ip.split(".").map(Number);
+			return (
+				parts[0] === 10 ||
+				(parts[0] === 172 && parts[1] >= 16 && parts[1] < 32) ||
+				(parts[0] === 192 && parts[1] === 168) ||
+				parts[0] === 127 ||
+				parts[0] === 0
+			);
+		} else if (net.isIPv6(ip)) {
+			return (
+				ip.startsWith("::") || ip.startsWith("fc00:") || ip.startsWith("fe80:")
+			);
+		}
+		return false;
+	});
+
+	if (isSSRF) {
+		ctx.status = 400;
+		ctx.body = { message: "Access to this URL is not allowed" };
 		return;
 	}
 
@@ -27,25 +65,13 @@ export async function proxyMedia(ctx: Koa.Context) {
 
 		const { mime, ext } = await detectType(path);
 		const isConvertibleImage = isMimeImage(mime, "sharp-convertible-image");
-		
-		/*if (
-            'static' in ctx.query ||
-            'preview' in ctx.query ||
-            'badge' in ctx.query
-        ) {
-            if (!isConvertibleImage) {
-                // 画像でないなら404でお茶を濁す
-                throw new StatusError('Unexpected mime', 404);
-            }
-        }*/
-
 
 		let image: IImage;
 
 		if ("static" in ctx.query && isConvertibleImage) {
 			image = await convertToWebp(path, 996, 560);
 		} else if ("preview" in ctx.query && isConvertibleImage) {
-			image = await convertToWebp(path, 996, 560);
+			image = await convertToWebp(path, 800, 800);
 		} else if ("badge" in ctx.query) {
 			if (!isConvertibleImage) {
 				// 画像でないなら404でお茶を濁す

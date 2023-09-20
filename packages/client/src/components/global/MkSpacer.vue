07775c518e17399099e9217215a037686dba8831
emtkmkk <emtk@mkkey.net>
2023-09-20 14:20:04 +0900

    packages/client/src/components/global/MkSpacer.vue を更新

--- packages/client/src/components/global/MkSpacer.vue
+++ packages/client/src/components/global/MkSpacer.vue
@@ -9,6 +9,7 @@
 <script lang="ts" setup>
 import { inject, onMounted, onUnmounted, ref } from "vue";
 import { deviceKind } from "@/scripts/device-kind";
+import { ui } from "@/config";
 
 const props = withDefaults(
 	defineProps<{
@@ -20,18 +21,22 @@ const props = withDefaults(
 		contentMax: null,
 		marginMin: 12,
 		marginMax: 24,
-	}
+	},
 );
 
-let ro: ResizeObserver;
-let root = $ref<HTMLElement>();
-let content = $ref<HTMLElement>();
-let margin = $ref(0);
+let ro: ResizeObserver,
+	root = ref<HTMLElement>(),
+	content = ref<HTMLElement>(),
+	margin = ref(0);
 const shouldSpacerMin = inject("shouldSpacerMin", false);
 
 const adjust = (rect: { width: number; height: number }) => {
 	if (shouldSpacerMin || deviceKind === "smartphone") {
-		margin = props.marginMin;
+		margin.value = props.marginMin;
+		return;
+	}
+	if (ui === "classic") {
+		margin.value = 12;
 		return;
 	}
 
@@ -39,9 +44,9 @@ const adjust = (rect: { width: number; height: number }) => {
 		rect.width > (props.contentMax ?? 0) ||
 		(rect.width > 360 && window.innerWidth > 400)
 	) {
-		margin = props.marginMax;
+		margin.value = props.marginMax;
 	} else {
-		margin = props.marginMin;
+		margin.value = props.marginMin;
 	}
 };
 
@@ -54,14 +59,14 @@ onMounted(() => {
 		});
 		*/
 		adjust({
-			width: root!.offsetWidth,
-			height: root!.offsetHeight,
+			width: root.value!.offsetWidth,
+			height: root.value!.offsetHeight,
 		});
 	});
-	ro.observe(root!);
+	ro.observe(root.value!);
 
 	if (props.contentMax) {
-		content!.style.maxWidth = `${props.contentMax}px`;
+		content.value!.style.maxWidth = `${props.contentMax}px`;
 	}
 });
 

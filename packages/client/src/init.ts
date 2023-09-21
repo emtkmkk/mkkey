71ca4e99ea18eb7839935051162f3bb16818520d
emtkmkk <emtk@mkkey.net>
2023-09-21 15:38:23 +0900

    revert c22cc188d5dea2af2bcacb37620eac340a77b677
    revert packages/client/src/init.ts を更新

--- packages/client/src/init.ts
+++ packages/client/src/init.ts
@@ -26,7 +26,6 @@ import {
 	version as vueVersion,
 	defineAsyncComponent,
 } from "vue";
-import Vue from "vue";
 import { compareVersions } from "compare-versions";
 import JSON5 from "json5";
 
@@ -97,7 +96,7 @@ import { isMobileData, initializeDetectNetworkChange } from '@/scripts/datasaver
 	});
 	
 
-	Vue.config.errorHandler = async (err, vm, info) => {
+	/*config.errorHandler = async (err, vm, info) => {
 		
 		const currentDate = new Date();
 		const formattedDate = currentDate.toLocaleDateString() + " " + currentDate.toLocaleTimeString();
@@ -113,7 +112,7 @@ import { isMobileData, initializeDetectNetworkChange } from '@/scripts/datasaver
 		}
 
 		await set("errorLog", currentLogs);
-	}
+	}*/
 	
 	if (_DEV_) {
 		console.warn("Development mode!!!");

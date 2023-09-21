d3e19e49cbe1548105a59ab5681a4f6101bd2da1
emtkmkk <emtk@mkkey.net>
2023-09-21 15:31:06 +0900

    packages/client/src/init.ts を更新

--- packages/client/src/init.ts
+++ packages/client/src/init.ts
@@ -96,7 +96,7 @@ import { isMobileData, initializeDetectNetworkChange } from '@/scripts/datasaver
 	});
 	
 
-	Vue.config.errorHandler = async (err, vm, info) => {
+	/*config.errorHandler = async (err, vm, info) => {
 		
 		const currentDate = new Date();
 		const formattedDate = currentDate.toLocaleDateString() + " " + currentDate.toLocaleTimeString();
@@ -112,7 +112,7 @@ import { isMobileData, initializeDetectNetworkChange } from '@/scripts/datasaver
 		}
 
 		await set("errorLog", currentLogs);
-	}
+	}*/
 	
 	if (_DEV_) {
 		console.warn("Development mode!!!");

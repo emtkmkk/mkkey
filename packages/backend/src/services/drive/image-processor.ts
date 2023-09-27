0fee923b03754a650339e8ebfff88585d946fb3d
emtkmkk <emtk@mkkey.net>
2023-09-27 14:22:43 +0900

    packages/backend/src/services/drive/image-processor.ts を更新

--- packages/backend/src/services/drive/image-processor.ts
+++ packages/backend/src/services/drive/image-processor.ts
@@ -6,6 +6,16 @@ export type IImage = {
 	type: string;
 };
 
+export const webpDefault: sharp.WebpOptions = {
+    quality: 85,
+    alphaQuality: 95,
+    lossless: false,
+    nearLossless: false,
+    smartSubsample: true,
+    mixed: true,
+    effort: 2,
+};
+
 /**
  * Convert to WebP
  *   with resize, remove metadata, resolve orientation, stop animation
@@ -14,26 +24,42 @@ export async function convertToWebp(
 	path: string,
 	width: number,
 	height: number,
-	quality: number = 85,
+	options: sharp.WebpOptions | number = {},
+	quality?: number | null,
 ): Promise<IImage> {
-	return convertSharpToWebp(await sharp(path), width, height, quality);
+	if (typeof options === 'number') {
+		quality = options;
+		options = { ...webpDefault, quality };
+	} else if (quality !== null && quality !== undefined) {
+		options = { ...webpDefault, quality };
+	} else {
+		options = { ...webpDefault, ...options };
+	}
+	return convertSharpToWebp(await sharp(path), width, height, options);
 }
 
 export async function convertSharpToWebp(
 	sharp: sharp.Sharp,
 	width: number,
 	height: number,
-	quality: number = 85,
+	options: sharp.WebpOptions | number = {},
+	quality?: number | null,
 ): Promise<IImage> {
+	if (typeof options === 'number') {
+		quality = options;
+		options = { ...webpDefault, quality };
+	} else if (quality !== null && quality !== undefined) {
+		options = { ...webpDefault, quality };
+	} else {
+		options = { ...webpDefault, ...options };
+	}
 	const data = await sharp
 		.resize(width, height, {
 			fit: "inside",
 			withoutEnlargement: true,
 		})
 		.rotate()
-		.webp({
-			quality,
-		})
+        .webp(options)
 		.toBuffer();
 
 	return {

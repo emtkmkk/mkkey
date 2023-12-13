da6110761f85412115673a197ea6e7ee097414d7
emtkmkk <emtk@mkkey.net>
2023-12-13 00:52:31 +0000

    staticmodeで/emoji/を叩く際に渡すクエリを正しくする

--- packages/client/src/scripts/get-static-image-url.ts
+++ packages/client/src/scripts/get-static-image-url.ts
@@ -2,13 +2,18 @@ import { url as instanceUrl } from "@/config";
 import * as url from "@/scripts/url";
 
 export function getStaticImageUrl(baseUrl: string): string {
-	const u = new URL(baseUrl);
+	const u = baseUrl.startsWith('http') ? new URL(baseUrl) : new URL(baseUrl, instanceUrl);
+	if (u.href.startsWith(`${instanceUrl}/emoji/`)) {
+		// もう既にemojiっぽそうだったらsearchParams付けるだけ
+		u.searchParams.set('static', '1');
+		return u.href;
+	}
 	if (u.href.startsWith(`${instanceUrl}/proxy/`)) {
 		// もう既にproxyっぽそうだったらsearchParams付けるだけ
 		u.searchParams.set("static", "1");
 		return u.href;
 	}
-	const dummy = `${u.host}${u.pathname}`; // 拡張子がないとキャッシュしてくれないCDNがあるので
+	const dummy = "static.webp"; // 拡張子がないとキャッシュしてくれないCDNがあるので
 	return `${instanceUrl}/proxy/${dummy}?${url.query({
 		url: u.href,
 		static: "1",

d1a8dc5562776ac2e0e98312e7828f8e0612bba5
emtkmkk <emtk@mkkey.net>
2023-12-13 00:53:05 +0000

    メディアプロキシに問い合わせる際のファイル名を変更

--- packages/client/src/scripts/media-proxy.ts
+++ packages/client/src/scripts/media-proxy.ts
@@ -2,7 +2,16 @@ import { query } from "@/scripts/url";
 import { url } from "@/config";
 
 export function getProxiedImageUrl(imageUrl: string, type?: "preview"): string {
-	return `${url}/proxy/image.webp?${query({
+
+	if (imageUrl.startsWith('/proxy/')) {
+		// もう既にproxyっぽそうだったらurlを取り出す
+		imageUrl = (new URL(imageUrl)).searchParams.get('url') ?? imageUrl;
+	}
+
+	return `${url}/proxy/${
+		type === 'preview' ? 'preview.webp'
+		: 'image.webp'
+	}?${query({
 		url: imageUrl,
 		fallback: "1",
 		...(type ? { [type]: "1" } : {}),

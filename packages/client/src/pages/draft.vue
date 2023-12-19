f121479ad781fa4d64dfc365e03d65a352079d02
emtkmkk <emtk@mkkey.net>
2023-12-19 06:52:21 +0000

    f

--- packages/client/src/pages/draft.vue
+++ packages/client/src/pages/draft.vue
@@ -23,7 +23,7 @@
 							`${draft.value.data.useCw ? `${draft.value.data.cw || "CW"} / ` : ""}${draft.value.data.text || ts._drafts.noText}`
 						}}
 					</div>
-					<div v-if="draft.value.data.visibility !== "public" || draft.value.data.localOnly || draft.value.data.files?.length || draft.value.data.poll || draft.value.data.quoteId" :class="$style.draftText">
+					<div v-if="draft.value.data.visibility !== 'public' || draft.value.data.localOnly || draft.value.data.files?.length || draft.value.data.poll || draft.value.data.quoteId" :class="$style.draftText">
 						{{
 							`${draft.value.data.visibility !== "public" && ts._visibility[draft.value.data.visibility] ? ts._visibility[draft.value.data.visibility] + " " : "" }${draft.value.data.localOnly ? ts._visibility.localAndFollower + " " : "" }${draft.value.data.quoteId ? ts._drafts.quote : ""}${draft.value.data.quoteId ? ts._drafts.quote : ""}${draft.value.data.poll ? ts._drafts.poll : ""}${draft.value.data.files?.length ? `${i18n.t("_drafts.files", { count: draft.value.data.files?.length })} ` : ""}`
 						}}

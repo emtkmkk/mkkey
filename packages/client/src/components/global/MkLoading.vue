873e1c71fddb9f99cadd5a490f7baebdd4296175
emtkmkk <emtk@mkkey.net>
2023-12-28 08:18:19 +0000

    ローディングゲージを進捗ゲージっぽくしてみる MAXになってもアイコンが出るだけ

--- packages/client/src/components/global/MkLoading.vue
+++ packages/client/src/components/global/MkLoading.vue
@@ -1,149 +1,173 @@
-<template>
-	<div
-		:class="[
-			$style.root,
-			{
-				[$style.inline]: inline,
-				[$style.colored]: colored,
-				[$style.mini]: mini,
-			},
-		]"
-	>
-		<div :class="$style.container" aria-hidden="true">
-			<svg
-				v-if="!isLongTime"
-				:class="[$style.spinner]"
-				viewBox="0 0 50 50"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<circle
-					:class="[$style.path]"
-					cx="25"
-					cy="25"
-					r="20"
-					fill="none"
-					stroke-width="6px"
-					style="fill: none; stroke: currentColor; stroke-width: 6px"
-				></circle>
-			</svg>
-			<img
-				v-if="isLongTime"
-				:class="[$style.spinner]"
-				:src="$instance.iconUrl"
-				alt="longlongloading"
-			/>
+	<template>
+		<div
+			:class="[
+				$style.root,
+				{
+					[$style.inline]: inline,
+					[$style.colored]: colored,
+					[$style.mini]: mini,
+				},
+			]"
+		>
+			<div :class="$style.container" aria-hidden="true">
+				<svg
+					v-if="!isLongTime"
+					:class="[$style.spinner]"
+					viewBox="0 0 50 50"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<circle
+							:class="[$style.path]"
+							cx="25"
+							cy="25"
+							r="20"
+							fill="none"
+							stroke-width="6px"
+							style="fill: none; stroke: currentColor; stroke-width: 6px; stroke-dasharray: 125.664; stroke-dashoffset: calc(125.664 * (1 - progress));"
+					></circle>
+				</svg>
+				<img
+					v-if="isLongTime"
+					:class="[$style.spinner]"
+					:src="$instance.iconUrl"
+					alt="longlongloading"
+				/>
+			</div>
 		</div>
-	</div>
-</template>
+	</template>
 
-<script lang="ts" setup>
-import { ref, onMounted, onBeforeUnmount } from "vue";
+	<script lang="ts" setup>
+	import { ref, onMounted, onBeforeUnmount } from "vue";
 
-const props = withDefaults(
-	defineProps<{
-		inline?: boolean;
-		colored?: boolean;
-		mini?: boolean;
-		em?: boolean;
-	}>(),
-	{
-		inline: false,
-		colored: true,
-		mini: false,
-		em: false,
-	}
-);
+	const props = withDefaults(
+		defineProps<{
+			inline?: boolean;
+			colored?: boolean;
+			mini?: boolean;
+			em?: boolean;
+		}>(),
+		{
+			inline: false,
+			colored: true,
+			mini: false,
+			em: false,
+		}
+	);
 
-const isLongTime = ref(false);
-let timerId = null;
+	const progress = ref(0);
+	const duration = 30000;
+	const finalDuration = 10000;
+	const isLongTime = ref(false);
+	let timerId: number | null = null;
+	let finalProgress = 0;
 
-onMounted(() => {
-	timerId = setTimeout(() => {
-	isLongTime.value = true;
-	}, 30000);
-});
+	onMounted(() => {
+		let startTime = Date.now();
 
-onBeforeUnmount(() => {
-	clearTimeout(timerId); // タイマーの解除
-});
-</script>
+		const updateProgress = () => {
+			const elapsedTime = Date.now() - startTime;
+			const finalTime = Math.max(elapsedTime - (duration - finalDuration), 0);
+			const randomIncrement = 0.0075 + Math.random() * 0.017;
 
-<style lang="scss" module>
-/* Credit to https://codepen.io/supah/pen/BjYLdW */
+			if (finalTime > 0) {
+				if (!finalProgress) finalProgress = progress.value
+				progress.value = finalProgress + (1 - finalProgress) * (finalTime / finalDuration);
+			} else {
+				progress.value += randomIncrement;
+			}
 
-@keyframes spin {
-	0% {
-		transform: rotate(0deg);
-	}
-	100% {
-		transform: rotate(360deg);
-	}
-}
+			if (progress.value < 1) {
+				timerId = setTimeout(updateProgress, 500);
+			} else {
+				isLongTime.value = true;
+			}
+		};
 
-@keyframes dash {
-	0% {
-		stroke-dasharray: 1, 150;
-		stroke-dashoffset: 0;
-	}
-	50% {
-		stroke-dasharray: 90, 150;
-		stroke-dashoffset: -35;
-	}
-	100% {
-		stroke-dasharray: 90, 150;
-		stroke-dashoffset: -124;
-	}
-}
+		// 進行状況の更新を開始
+		updateProgress();
+	});
 
-.root {
-	padding: 32px;
-	text-align: center;
-	cursor: wait;
+	onBeforeUnmount(() => {
+		clearTimeout(timerId); // タイマーの解除
+	});
+	</script>
 
-	--size: 40px;
+	<style lang="scss" module>
+	/* Credit to https://codepen.io/supah/pen/BjYLdW */
 
-	&.colored {
-		color: var(--accent);
+	@keyframes spin {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
 	}
 
-	&.inline {
-		display: inline;
-		padding: 0;
-		--size: 32px;
+	@keyframes dash {
+		0% {
+			stroke-dasharray: 1, 150;
+			stroke-dashoffset: 0;
+		}
+		50% {
+			stroke-dasharray: 90, 150;
+			stroke-dashoffset: -35;
+		}
+		100% {
+			stroke-dasharray: 90, 150;
+			stroke-dashoffset: -124;
+		}
 	}
 
-	&.mini {
-		padding: 16px;
-		--size: 32px;
+	.root {
+		padding: 32px;
+		text-align: center;
+		cursor: wait;
+
+		--size: 40px;
+
+		&.colored {
+			color: var(--accent);
+		}
+
+		&.inline {
+			display: inline;
+			padding: 0;
+			--size: 32px;
+		}
+
+		&.mini {
+			padding: 16px;
+			--size: 32px;
+		}
+		&.em {
+			display: inline-block;
+			vertical-align: middle;
+			padding: 0;
+			--size: 1em;
+		}
 	}
-	&.em {
-		display: inline-block;
-		vertical-align: middle;
-		padding: 0;
-		--size: 1em;
+
+	.container {
+		position: relative;
+		width: var(--size);
+		height: var(--size);
+		margin: 0 auto;
 	}
-}
 
-.container {
-	position: relative;
-	width: var(--size);
-	height: var(--size);
-	margin: 0 auto;
-}
+	.spinner {
+		position: absolute;
+		top: 0;
+		left: 0;
+		z-index: 999;
+		width: var(--size);
+		height: var(--size);
+		//animation: spin 2s linear infinite;
+	}
 
-.spinner {
-	position: absolute;
-	top: 0;
-	left: 0;
-	z-index: 999;
-	width: var(--size);
-	height: var(--size);
-	animation: spin 2s linear infinite;
-}
-
-.path {
-	stroke: var(--accent);
-	stroke-linecap: round;
-	animation: dash 1.2s ease-in-out infinite;
-}
-</style>
+	.path {
+		stroke: var(--accent);
+		stroke-linecap: round;
+		//animation: dash 1.2s ease-in-out infinite;
+	}
+	</style>

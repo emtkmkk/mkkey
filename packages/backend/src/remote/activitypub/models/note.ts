81162933cb930dba86c6172a079418a60bc1f774
emtkmkk <emtk@mkkey.net>
2024-01-10 02:09:18 +0000

    escape

--- packages/backend/src/remote/activitypub/models/note.ts
+++ packages/backend/src/remote/activitypub/models/note.ts
@@ -316,7 +316,7 @@ export async function createNote(
 		text = htmlToMfm(note.content, note.tag);
 	}
 	if (quote && text) {
-		let reg = new RegExp(`(\n\n|^)[^\n]+${quote.uri ? quote.uri : `${config.url}/notes/${quote.id}}`}$`,"i");
+		let reg = new RegExp(`(\n\n|^)[^\n]+${quote.uri ? quote.uri.replaceAll("/","\\/") : `${config.url}/notes/${quote.id}}`.replaceAll("/","\\/")}$`,"i");
 		text = text.replace(reg,"");
 	}
 

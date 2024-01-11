cd4ceb19d4c76330c08ba5682b8d82008ec9f312
emtkmkk <emtk@mkkey.net>
2024-01-11 02:50:20 +0000

    StickyContainerの修正

--- packages/client/src/components/global/MkStickyContainer.vue
+++ packages/client/src/components/global/MkStickyContainer.vue
@@ -14,34 +14,40 @@
 
 <script lang="ts">
 // なんか動かない
-//const CURRENT_STICKY_TOP = Symbol('CURRENT_STICKY_TOP');
+//const CURRENT_STICKY_TOP = Symbol("CURRENT_STICKY_TOP");
 const CURRENT_STICKY_TOP = "CURRENT_STICKY_TOP";
 </script>
 
 <script lang="ts" setup>
 import { onMounted, onUnmounted, provide, inject, Ref, ref, watch, shallowRef } from "vue";
 
-const rootEl = $shallowRef<HTMLElement>();
-const headerEl = $shallowRef<HTMLElement>();
-const bodyEl = $shallowRef<HTMLElement>();
-const footerEl = $shallowRef<HTMLElement>();
+const rootEl = shallowRef<HTMLElement>();
+const headerEl = shallowRef<HTMLElement>();
+const bodyEl = shallowRef<HTMLElement>();
+const footerEl = shallowRef<HTMLElement>();
 
-let headerHeight = $ref<string | undefined>();
-let childStickyTop = $ref(0);
+const headerHeight = ref<string | undefined>();
+const childStickyTop = ref(0);
 const parentStickyTop = inject<Ref<number>>(CURRENT_STICKY_TOP, ref(0));
-provide(CURRENT_STICKY_TOP, $$(childStickyTop));
+provide(CURRENT_STICKY_TOP, childStickyTop);
 
-let footerHeight = $ref<string | undefined>();
-let childStickyBottom = $ref(0);
+const footerHeight = ref<string | undefined>();
+const childStickyBottom = ref(0);
 const parentStickyBottom = inject<Ref<number>>("CURRENT_STICKY_BOTTOM", ref(0));
-provide("CURRENT_STICKY_BOTTOM", $$(childStickyBottom));
-
+provide("CURRENT_STICKY_BOTTOM", childStickyBottom);
 
 const calc = () => {
-	childStickyTop = parentStickyTop.value + headerEl.offsetHeight;
-	headerHeight = headerEl.offsetHeight.toString();
-	childStickyBottom = parentStickyBottom.value + footerEl.offsetHeight;
-	footerHeight = footerEl.offsetHeight.toString();
+	// コンポーネントが表示されてないけどKeepAliveで残ってる場合などは null になる
+	if (headerEl.value != null) {
+		childStickyTop.value = parentStickyTop.value + headerEl.value.offsetHeight;
+		headerHeight.value = headerEl.value.offsetHeight.toString();
+	}
+
+	// コンポーネントが表示されてないけどKeepAliveで残ってる場合などは null になる
+	if (footerEl.value != null) {
+		childStickyBottom.value = parentStickyBottom.value + footerEl.value.offsetHeight;
+		footerHeight.value = footerEl.value.offsetHeight.toString();
+	}
 };
 
 const observer = new ResizeObserver(() => {
@@ -55,40 +61,36 @@ onMounted(() => {
 
 	watch([parentStickyTop, parentStickyBottom], calc);
 
-	watch(
-		$$(childStickyTop),
-		() => {
-			bodyEl.style.setProperty("--stickyTop", `${childStickyTop}px`);
-		},
-		{
-			immediate: true,
-		}
-	);
-	watch(
-		$$(childStickyBottom),
-		() => {
-			bodyEl.style.setProperty('--stickyBottom', `${childStickyBottom}px`);
-		},
-		{
-			immediate: true,
-		}
-	);
+	watch(childStickyTop, () => {
+		bodyEl.value.style.setProperty("--stickyTop", `${childStickyTop.value}px`);
+	}, {
+		immediate: true,
+	});
 
+	watch(childStickyBottom, () => {
+		bodyEl.value.style.setProperty("--stickyBottom", `${childStickyBottom.value}px`);
+	}, {
+		immediate: true,
+	});
 
-	headerEl.style.position = "sticky";
-	headerEl.style.top = "var(--stickyTop, 0)";
-	headerEl.style.zIndex = "1000";
-	
-	footerEl.style.position = 'sticky';
-	footerEl.style.bottom = 'var(--stickyBottom, 0)';
-	footerEl.style.zIndex = '1000';
-	
-	observer.observe(headerEl);
-	observer.observe(footerEl);
+	headerEl.value.style.position = "sticky";
+	headerEl.value.style.top = "var(--stickyTop, 0)";
+	headerEl.value.style.zIndex = "1000";
+
+	footerEl.value.style.position = "sticky";
+	footerEl.value.style.bottom = "var(--stickyBottom, 0)";
+	footerEl.value.style.zIndex = "1000";
+
+	observer.observe(headerEl.value);
+	observer.observe(footerEl.value);
 });
 
 onUnmounted(() => {
 	observer.disconnect();
+});
+
+defineExpose({
+	rootEl: rootEl,
 });
 </script>
 

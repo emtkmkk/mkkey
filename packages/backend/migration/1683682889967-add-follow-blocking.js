abf3aa4b1d4098e65736d85bacf945c81cdb6a52
emtkmkk <emtk@mkkey.net>
2024-01-22 02:27:47 +0000

    fix

--- packages/backend/migration/1683682889967-add-follow-blocking.js
+++ packages/backend/migration/1683682889967-add-follow-blocking.js
@@ -8,13 +8,13 @@ export class addFollowBlocking1683682889967 {
 			`CREATE TABLE "follow_blocking" ("id" character varying(32) NOT NULL, "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL, "blockeeId" character varying(32) NOT NULL, "blockerId" character varying(32) NOT NULL, CONSTRAINT "PK_followblocking_id" PRIMARY KEY ("id"))`,
 		);
 		await queryRunner.query(
-			`CREATE INDEX "IDX_follow_blocking_createdAt" ON "muting" ("createdAt") `,
+			`CREATE INDEX "IDX_follow_blocking_createdAt" ON "follow_blocking" ("createdAt") `,
 		);
 		await queryRunner.query(
-			`CREATE INDEX "IDX_follow_blocking_muteeId" ON "muting" ("blockeeId") `,
+			`CREATE INDEX "IDX_follow_blocking_muteeId" ON "follow_blocking" ("blockeeId") `,
 		);
 		await queryRunner.query(
-			`CREATE INDEX "IDX_follow_blocking_muterId" ON "muting" ("blockerId") `,
+			`CREATE INDEX "IDX_follow_blocking_muterId" ON "follow_blocking" ("blockerId") `,
 		);
 	}
 

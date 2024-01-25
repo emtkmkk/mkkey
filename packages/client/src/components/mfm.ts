19f03ec415e3238df3928f17e02c0fd2b45ac38b
emtkmkk <emtk@mkkey.net>
2024-01-25 12:30:14 +0900

    こっちも修正

--- packages/client/src/components/mfm.ts
+++ packages/client/src/components/mfm.ts
@@ -381,7 +381,7 @@ export default defineComponent({
 								}
 								case 'unixtime': {
 									const child = token.children[0];
-									const unixtime = parseInt(child.type === 'text' ? child.props.text : '');
+									const unixtime = Number(child.type === 'text' ? child.props.text : '');
 									return h('span', {
 										style: 'display: inline-block; font-size: 90%; border: solid 1px var(--divider); border-radius: 999px; padding: 4px 10px 4px 6px;',
 									}, [

3d4712f73d46b7a1427fc42806e7607d76ed1b25
emtkmkk <emtk@mkkey.net>
2024-02-13 08:31:44 +0900

    Update mfm.ts

--- packages/client/src/components/mfm.ts
+++ packages/client/src/components/mfm.ts
@@ -387,7 +387,6 @@ export default defineComponent({
 								}
 								case "border": {
 									let color = token.props.args.color;
-									va;
 									color = validColor(color) ? `#${color}` : "var(--accent)";
 									let b_style = token.props.args.style;
 									if (

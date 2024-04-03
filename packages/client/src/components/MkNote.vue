64efb82c092b4b0fb34ef2eed82f71e69868abc3
emtkmkk <emtk@mkkey.net>
2024-04-03 04:49:12 +0000

    見えない投稿の場合は空RTでも引用のように見せる

--- packages/client/src/components/MkNote.vue
+++ packages/client/src/components/MkNote.vue
@@ -523,7 +523,8 @@ const isRenote =
 	note.renoteId != null &&
 	note.text == null &&
 	note.fileIds.length === 0 &&
-	note.poll == null;
+	note.poll == null; &&
+	!note.invisible
 
 const isQuote = note.renoteId != null && !isRenote;
 

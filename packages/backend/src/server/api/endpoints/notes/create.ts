b19ef9f6e0cf8a8dc35aee7b188d569c0e76844e
emtkmkk <emtk@mkkey.net>
2024-05-16 19:25:37 +0900

    urlとしてパースできるかをチェック

--- packages/backend/src/server/api/endpoints/notes/create.ts
+++ packages/backend/src/server/api/endpoints/notes/create.ts
@@ -335,14 +335,14 @@ export default define(meta, paramDef, async (ps, user) => {
 		for (const url of ps.fileUrls) {
 			let file;
 			if (typeof url === "string") {
-				if (url.trim()) {
+				if (url.trim() && URL.canParse(url.trim())) {
 					file = await uploadFromUrl({
 						url: url.trim(),
 						user
 					})
 				}
 			} else {
-				if (url.url.trim()) {
+				if (url.url.trim() && URL.canParse(url.url.trim()) {
 					file = await uploadFromUrl({
 						url: url.url.trim(),
 						user,

3b6863ea31705ba70f8f4b6a3756d7ea4e14e3d9
emtkmkk <emtk@mkkey.net>
2024-05-21 16:30:47 +0900

    fix

--- packages/client/src/scripts/sleep.ts
+++ packages/client/src/scripts/sleep.ts
@@ -14,7 +14,7 @@ export async function sleep() {
 
 	date.setMinutes(date.getMinutes() + 120)
 
-	localStorage.setItem("sleepTime", date.toISOString);
+	localStorage.setItem("sleepTime", date.toISOString());
 
 	location.reload();
 

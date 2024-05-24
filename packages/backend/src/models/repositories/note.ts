6fc543dae28c95e040e3e05f550b6a3fca8449c9
emtkmkk <emtk@mkkey.net>
2024-05-24 04:12:06 +0000

    fix

--- packages/backend/src/models/repositories/note.ts
+++ packages/backend/src/models/repositories/note.ts
@@ -245,7 +245,10 @@ export const NoteRepository = db.getRepository(Note).extend({
 
 		const reactions =
 			note.isPublicLikeList || meId === note.userId
-				? note.reactions
+				? {...(myReactions?.myReactions.length ? myReactions.myReactions.reduce(
+					(acc, curr) => ((acc[curr] = 1), acc),
+					{},
+				) : {}), ...note.reactions}
 				: myReactions?.myReactions
 				? myReactions.myReactions.reduce(
 						(acc, curr) => ((acc[curr] = 1), acc),

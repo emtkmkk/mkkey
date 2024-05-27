9789b09c2e078cb13949e55f47bb7d57c2c6ff73
emtkmkk <emtk@mkkey.net>
2024-05-27 15:01:02 +0900

    Update 1683682889976-addMiniSilenced.js

--- packages/backend/migration/1683682889976-addMiniSilenced.js
+++ packages/backend/migration/1683682889976-addMiniSilenced.js
@@ -3,7 +3,7 @@ export class addMiniSilenced1683682889976 {
 
 	async up(queryRunner) {
 		await queryRunner.query(
-			`ALTER TABLE "user" ADD "isMiniSilenced" boolean NOT NULL DEFAULT false,`,
+			`ALTER TABLE "user" ADD "isMiniSilenced" boolean NOT NULL DEFAULT false`,
 		);
 	}
 

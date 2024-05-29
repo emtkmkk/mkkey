379126a7ad3f26f30848f92db394346b2c20b340
emtkmkk <emtk@mkkey.net>
2024-05-29 07:20:05 +0000

    codeにコピーボタンを追加

--- packages/client/src/components/MkCode.vue
+++ packages/client/src/components/MkCode.vue
@@ -1,17 +1,45 @@
 <template>
-	<XCode :code="code" :lang="lang" :inline="inline" />
+	<div :class="$style.codeBlockRoot">
+		<button :class="$style.codeBlockCopyButton" class="_button" @click="copy">
+			<i class="ph-bold ph-lg ph-copy"></i>
+		</button>
+		<XCode :code="code" :lang="lang" :inline="inline" />
+	</div>
 </template>
 
 <script lang="ts" setup>
+import copyToClipboard from "@/scripts/copy-to-clipboard";
 import { defineAsyncComponent } from "vue";
+import * as os from "@/os";
 
-defineProps<{
+const props = defineProps<{
 	code: string;
 	lang?: string;
 	inline?: boolean;
 }>();
 
+function copy() {
+	copyToClipboard(props.code);
+	os.success();
+}
 const XCode = defineAsyncComponent(
 	() => import("@/components/MkCode.core.vue")
 );
 </script>
+
+<style module lang="scss">
+.codeBlockRoot {
+	position: relative;
+}
+
+.codeBlockCopyButton {
+	position: absolute;
+	top: 8px;
+	right: 8px;
+	opacity: 0.5;
+
+	&:hover {
+		opacity: 0.8;
+	}
+}
+</style>

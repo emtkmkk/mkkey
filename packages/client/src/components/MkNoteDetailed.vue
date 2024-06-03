c8477b5cc71db9f88a7388290997dcacd9e195f4
emtkmkk <emtk@mkkey.net>
2024-06-03 07:21:29 +0000

    fix

--- packages/client/src/components/MkNoteDetailed.vue
+++ packages/client/src/components/MkNoteDetailed.vue
@@ -390,9 +390,9 @@ onMounted(async () => {
 					type: "question",
 					text: "この投稿には高画質のオリジナル画像が添付されています。\n画像を拡大表示した際に常に高画質のオリジナル画像を表示しますか？\n\nはいにした場合、通信量が増加します。\nいいえにした場合、横幅か縦幅の長い方が2048pxまで縮小されます。（画像ダウンロードの際の画質にも影響します。）\n\nここで選択した設定は次回以降のデフォルトとして設定されます。",
 				})
-				defaultStore.set("loadOriginalImages", canceled);
+				defaultStore.set("loadOriginalImages", !canceled);
 				defaultStore.set("confirmShowImgCompress", true);
-				if (canceled) location.reload();
+				if (!canceled) location.reload();
 	}
 });
 

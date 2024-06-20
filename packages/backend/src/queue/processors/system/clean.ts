d050480dc0bf3e92e32243569cc4cd8b2aa440dd
emtkmkk <emtk@mkkey.net>
2024-06-20 10:47:24 +0000

    処理を変更

--- packages/backend/src/queue/processors/system/clean.ts
+++ packages/backend/src/queue/processors/system/clean.ts
@@ -20,20 +20,49 @@ export async function clean(
 	logger.succ("UserIps Cleaned.");
 
 	logger.info("Notes Cleaning...");
-	const result = await Notes.createQueryBuilder("note")
-		.delete()
-		.where("createdAt < :date", { date: new Date(Date.now() - 1000 * 60 * 60 * 24 * 60) })
-		.andWhere("renoteCount = :renoteCount", { renoteCount: 0 })
-		.andWhere("repliesCount = :repliesCount", { repliesCount: 0 })
-		.andWhere("score = :score", { score: 0 })
-		.andWhere("userHost IS NOT NULL")
-    .andWhere(new Brackets(qb => {
-        qb.where("visibility = :public", { public: 'public' })
-          .orWhere("visibility = :home", { home: 'home' });
-    }))
-		.execute();
-	
-	console.log(`Notes Cleaned. (${result.affected})`);
+
+	{
+		let deleteCount = 0;
+		let failedCount = 0;
+		// Delete notes
+		let cursor: Note["id"] | null = null;
+
+		while (true) {
+			const notes = (await Notes.createQueryBuilder('note')
+			.where("note.createdAt < :date", { date: new Date(Date.now() - 1000 * 60 * 60 * 24 * 60) })
+			.andWhere("note.repliesCount = :repliesCount", { repliesCount: 0 })
+			.andWhere("note.score = :score", { score: 0 })
+			.andWhere("note.userHost IS NOT NULL")
+			.andWhere(new Brackets(qb => {
+					qb.where("note.visibility = :public", { public: 'public' })
+						.orWhere("note.visibility = :home", { home: 'home' });
+			}))
+			.andWhere(cursor ? "note.id > :cursor" : "1=1", { cursor })
+			.orderBy("note.id", "ASC")
+			.take(100)
+			.getMany()) as Note[];
+
+			if (notes.length === 0) {
+				break;
+			}
+
+			cursor = notes[notes.length - 1].id;
+
+			try {
+				await Notes.delete(notes.map((note) => note.id));
+				deleteCount += notes.length;
+			} catch {
+				failedCount += notes.length;
+			}
+		}
+
+		if (deleteCount + failedCount)
+			logger.succ(
+				`Notes Cleaned. (${deleteCount}${
+					failedCount ? ` / ${failedCount}` : ""
+				})`,
+			);
+	}
 
 	done();
 }

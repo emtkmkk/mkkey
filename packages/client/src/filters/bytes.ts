811cf0bde0c85d88fef92381eb830416620821d6
emtkmkk <emtk@mkkey.net>
2024-07-04 02:57:02 +0000

    最低表示桁数を指定できるように

--- packages/client/src/filters/bytes.ts
+++ packages/client/src/filters/bytes.ts
@@ -1,13 +1,16 @@
-export default (v, digits = 0) => {
+export default (v, digits = 0, minDigits = 3) => {
 	if (v == null) return "?";
 	const sizes = ["B", "KB", "MB", "GB", "TB"];
 	if (v === 0) return "0";
 	const isMinus = v < 0;
 	if (isMinus) v = -v;
-	const i = Math.floor(Math.log(v) / Math.log(1024));
+	let i = Math.floor(Math.log(v) / Math.log(1024));
+	while (i <= 0 || (v / 1024 ** i).toFixed(digits).replace(".","").length < minDigits) {
+		i -= 1
+	}
 	return (
 		(isMinus ? "-" : "") +
-		(v / Math.pow(1024, i)).toFixed(digits).replace(/\.0+$/, "") +
+		(v / 1024 ** i).toFixed(digits).replace(/\.0+$/, "") +
 		sizes[i]
 	);
 };

ece5e79bd7cc1eeb774ef06640735bc47c121c6f
emtkmkk <emtk@mkkey.net>
2024-07-05 23:54:46 +0900

    Update instance.ts

--- packages/client/src/instance.ts
+++ packages/client/src/instance.ts
@@ -20,7 +20,7 @@ export const instance: Misskey.entities.InstanceMetadata = reactive(
 );
 
 if (localStorage.getItem("followCategories")?.includes("object Object")) localStorage.setItem("followCategories", "[]") 
-export let followCategories = $ref(JSON.parse(localStorage.getItem("followCategories") ?? "[]"));
+export let followCategories = $ref(localStorage.getItem("followCategories")?.includes("object Object") ? [] : JSON.parse(localStorage.getItem("followCategories") ?? "[]"));
 
 stream.on("emojiAdded", (emojiData) => {
 	instance.emojis = [emojiData.emoji, ...instance.emojis];

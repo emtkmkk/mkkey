f6796ee4962d4935ebe89a97369f6ebb06f4df5e
emtkmkk <emtk@mkkey.net>
2024-07-09 18:51:04 +0900

    Update instance.ts

--- packages/client/src/instance.ts
+++ packages/client/src/instance.ts
@@ -82,7 +82,7 @@ export async function fetchCustomCategory() {
 	localStorage.setItem("followCategoriesTime", String(Date.now()))
 }
 
-export function sortCustomCategory {
+export function sortCustomCategory() {
 	followCategories　= defaultStore.state.followCategories.map((x) => followCategories.find((y) => x === y.id)).filter(Boolean);
 	localStorage.setItem("followCategories", JSON.stringify(followCategories))
 }

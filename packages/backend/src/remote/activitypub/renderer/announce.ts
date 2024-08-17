ddf7fba0d16591490b5cf48b4f4ee74ffe337465
emtkmkk <emtk@mkkey.net>
2024-08-18 04:29:37 +0900

    Update announce.ts

--- packages/backend/src/remote/activitypub/renderer/announce.ts
+++ packages/backend/src/remote/activitypub/renderer/announce.ts
@@ -4,9 +4,14 @@ import type { Note } from "@/models/entities/note.js";
 export default (object: any, note: Note) => {
 	const attributedTo = `${config.url}/users/${note.userId}`;
 
-	const mentions = (
-		JSON.parse(note.mentionedRemoteUsers) as IMentionedRemoteUsers
-	).map((x) => x.uri);
+	let mentions: string[] = [];
+	try {
+		mentions = (
+			JSON.parse(note.mentionedRemoteUsers) as IMentionedRemoteUsers
+		)?.map((x) => x.uri);
+	} catch(e) {
+		console.log(e);
+	}
 
 	let to: string[] = [];
 	let cc: string[] = [];

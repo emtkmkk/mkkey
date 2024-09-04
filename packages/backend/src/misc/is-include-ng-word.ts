39a428d7a85a3ba10624f397906949f9a16c7e6d
emtkmkk <emtk@mkkey.net>
2024-09-04 17:43:35 +0900

    完全な空白検知

--- packages/backend/src/misc/is-include-ng-word.ts
+++ packages/backend/src/misc/is-include-ng-word.ts
@@ -57,7 +57,7 @@ export function isIncludeNgWord(txt: string): string {
 
 	const text = txt
 		.toLowerCase()
-		.replaceAll(/\s/g, "")
+		.replaceAll(/[\u0009-\u000d\u001c-\u0020\u11a3-\u11a7\u1680\u180e\u2000-\u200f\u202f\u205f\u2060\u3000\u3164\ufeff\u034f\u2028\u2029\u202a-\u202e\u2061-\u2063\ufeff]/g, "")
 		.replaceAll(/[!-\/:-@[-`{-~]+/g, "")
 		.replace(/[ァ-ン]/g, function (match) {
 			var chr = match.charCodeAt(0) - 0x60;

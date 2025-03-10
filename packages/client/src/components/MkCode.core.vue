e03d34c5b61c64b2b3018a69eeb6ab75143f1428
emtkmkk <emtk@mkkey.net>
2025-03-10 13:17:58 +0900

    code要素にスクロールしない属性を追加

--- packages/client/src/components/MkCode.core.vue
+++ packages/client/src/components/MkCode.core.vue
@@ -1,9 +1,19 @@
 <template>
-	<code v-if="inline" :class="`language-${prismLang}`" v-html="html"></code>
+	<code
+		v-if="inline"
+		:class="`language-${prismLang}`"
+		v-html="html"
+		@touchstart.stop
+		@touchmove.stop
+	></code>
 	<pre
 		v-else
 		:class="`language-${prismLang}`"
-	><code :class="`language-${prismLang}`" v-html="html"></code></pre>
+		@touchstart.stop
+		@touchmove.stop
+	>
+		<code :class="`language-${prismLang}`" v-html="html"></code>
+	</pre>
 </template>
 
 <script lang="ts" setup>
